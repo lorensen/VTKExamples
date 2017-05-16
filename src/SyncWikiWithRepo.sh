e75f7b668d7ae1bc912de1a03205b137141ed1ae
Bill Lorensen <bill.lorensen@gmail.com>
2017-05-16 14:35:53 -0400

    SYNC: Files modified on wiki.

--- src/SyncWikiWithRepo.sh
+++ src/SyncWikiWithRepo.sh
@@ -6,7 +6,7 @@
 # Make sure the wiki is up
 echo "Synchronizing the wikiexamples repository with the repository."
 HOST=www.kitware.com
-REPO=https://gitlab.kitware.com/lorensen/VTKExamples
+REPO=https://github.kitware.com/lorensen/VTKExamples
 echo "0) Can we access the wiki?"
 ping -c 1 $HOST &> /dev/null
 if test "${?}" != 0
@@ -17,20 +17,16 @@ fi
 
 echo "1) Pull updates from master repositories"
 git pull
-(cd wiki; git pull)
 if ( test -d Tarballs ); then
   (cd Tarballs; git checkout .)
   (cd Tarballs; git pull origin master)
 fi
 
-echo "2) Remove all files from local wiki repository"
-(cd wiki; find . -name \*md -exec rm {} \;)
-
 echo "3) Scrape the repo"
-./Admin/ScrapeRepo . ./wiki ${REPO}
+./Admin/ScrapeRepo  ../src ../docs ${REPO}
 
 echo "3.1) Check for a successful scrape"
-pushd wiki
+pushd ../docs
 count=$((`find . -name \*.md | wc -l`))
 popd
 expected=800
@@ -42,15 +38,15 @@ if test $count -lt $expected; then
 fi
 
 echo "4) Process modified files"
-(cd wiki; git commit -m "SYNC: Files modified on wiki." `git status | grep modified: | cut -d":" -f2,2`)
+git commit -m "SYNC: Files modified on wiki." `git status | grep modified: | cut -d":" -f2,2`
 
 echo "5) Process new files"
-(cd wiki; find . "(" -name \*.md ")" -exec git add {} \;)
-(cd wiki; git commit -m "SYNC: Files added to wiki.")
+find . "(" -name \*.md ")" -exec git add {} \;
+git commit -m "SYNC: Files added to wiki."
 
 echo "6) Process deleted files"
-(cd wiki; git rm `git status | grep deleted: | cut -d":" -f2,2`)
-(cd wiki; git commit -m "SYNC: Files deleted (or moved) from wiki.")
+git rm `git status | grep deleted: | cut -d":" -f2,2`
+git commit -m "SYNC: Files deleted (or moved) from wiki."
 
 echo "7) Update tarballs and push to tarball repo"
 if ( test -d Tarballs ); then
@@ -60,5 +56,5 @@ if ( test -d Tarballs ); then
 fi
 
 echo "8) Push the changes"
-(cd wiki; git push)
+#git push
 
