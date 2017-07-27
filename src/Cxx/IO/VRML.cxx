8c1faeff1d5c8aa22c5ca57dea252d8b64b34af2
Bill Lorensen <bill.lorensen@gmail.com>
2017-07-27 11:29:40 -0400

    ENH: Improved VRML example

--- src/Cxx/IO/VRML.cxx
+++ src/Cxx/IO/VRML.cxx
@@ -1,10 +1,12 @@
+#include <vtkSmartPointer.h>
+#include <vtkVRMLImporter.h>
+
+#include <vtkActorCollection.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
-#include <vtkVRMLImporter.h>
-#include <vtkDataSet.h>
-#include <vtkPolyData.h>
-#include <vtkSmartPointer.h>
+
+#include <vtkNamedColors.h>
 
 int main ( int argc, char *argv[])
 {
@@ -17,19 +19,33 @@ int main ( int argc, char *argv[])
   std::string filename = argv[1];
   std::cout << "Reading " << filename << std::endl;
 
-  vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+
+  vtkSmartPointer<vtkRenderer> renderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
 
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
   renderWindowInteractor->SetRenderWindow(renderWindow);
 
   // VRML Import
-  vtkSmartPointer<vtkVRMLImporter> importer = vtkSmartPointer<vtkVRMLImporter>::New();
+  vtkSmartPointer<vtkVRMLImporter> importer =
+    vtkSmartPointer<vtkVRMLImporter>::New();
   importer->SetFileName ( filename.c_str() );
   importer->SetRenderWindow(renderWindow);
   importer->Update();
 
+  vtkSmartPointer<vtkActorCollection> actors =
+    vtkSmartPointer<vtkActorCollection>::New();
+  actors = renderer->GetActors();
+  std::cout << "There are " << actors->GetNumberOfItems() << " actors" << std::endl;
+
+  renderer->SetBackground(colors->GetColor3d("Burlywood").GetData());
+  renderWindow->SetSize(512, 512);
   renderWindow->Render();
   renderWindowInteractor->Start();
 
