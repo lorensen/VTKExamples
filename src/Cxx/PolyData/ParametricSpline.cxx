c01048afb6f2e0eadcb64627d3b2c75449df2f4d
Bill Lorensen <bill.lorensen@gmail.com>
2017-08-03 09:59:59 -0400

    ENH: Better colors and point rendering.

--- src/Cxx/PolyData/ParametricSpline.cxx
+++ src/Cxx/PolyData/ParametricSpline.cxx
@@ -1,19 +1,30 @@
 #include <vtkSmartPointer.h>
+#include <vtkParametricFunctionSource.h>
+#include <vtkParametricSpline.h>
+
 #include <vtkCellArray.h>
 #include <vtkCellData.h>
 #include <vtkDoubleArray.h>
 #include <vtkPoints.h>
-#include <vtkParametricSpline.h>
 #include <vtkPolyData.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkProperty.h>
+
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
-#include <vtkParametricFunctionSource.h>
+#include <vtkGlyph3DMapper.h>
+
+#include <vtkSphereSource.h>
+#include <vtkNamedColors.h>
 
 int main(int, char *[])
 {
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+
   // Create three points. We will join (Origin and P0) with a red line and (Origin and P1) with a green line
   double origin[3] = {0.0, 0.0, 0.0};
   double p0[3] = {1.0, 0.0, 0.0};
@@ -38,6 +49,12 @@ int main(int, char *[])
   functionSource->SetParametricFunction(spline);
   functionSource->Update();
 
+  vtkSmartPointer<vtkSphereSource> sphere =
+    vtkSmartPointer<vtkSphereSource>::New();
+  sphere->SetPhiResolution(21);
+  sphere->SetThetaResolution(21);
+  sphere->SetRadius(.1);
+
   // Setup actor and mapper
   vtkSmartPointer<vtkPolyDataMapper> mapper = 
     vtkSmartPointer<vtkPolyDataMapper>::New();
@@ -46,7 +63,24 @@ int main(int, char *[])
   vtkSmartPointer<vtkActor> actor = 
     vtkSmartPointer<vtkActor>::New();
   actor->SetMapper(mapper);
+  actor->GetProperty()->SetColor(colors->GetColor3d("DarkSlateGrey").GetData());
+  actor->GetProperty()->SetLineWidth(3.0);
   
+  // Create a polydata to store everything in
+  vtkSmartPointer<vtkPolyData> polyData =
+    vtkSmartPointer<vtkPolyData>::New();
+  polyData->SetPoints(points);
+
+  vtkSmartPointer<vtkGlyph3DMapper> pointMapper =
+    vtkSmartPointer<vtkGlyph3DMapper>::New();
+  pointMapper->SetInputData(polyData);
+  pointMapper->SetSourceConnection(sphere->GetOutputPort());
+
+  vtkSmartPointer<vtkActor> pointActor =
+    vtkSmartPointer<vtkActor>::New();
+  pointActor->SetMapper(pointMapper);
+  pointActor->GetProperty()->SetColor(colors->GetColor3d("Peacock").GetData());
+
   // Setup render window, renderer, and interactor
   vtkSmartPointer<vtkRenderer> renderer = 
     vtkSmartPointer<vtkRenderer>::New();
@@ -57,6 +91,8 @@ int main(int, char *[])
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
   renderWindowInteractor->SetRenderWindow(renderWindow);
   renderer->AddActor(actor);
+  renderer->AddActor(pointActor);
+  renderer->SetBackground(colors->GetColor3d("Silver").GetData());
 
   renderWindow->Render();
   renderWindowInteractor->Start();
