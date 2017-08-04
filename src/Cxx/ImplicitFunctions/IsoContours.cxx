7bb184cb9e483b92713bdd2c7ac23edf91dcf74e
Bill Lorensen <bill.lorensen@gmail.com>
2017-08-04 08:15:52 -0400

    ENH: Cleaned up IsoContours example

--- src/Cxx/ImplicitFunctions/IsoContours.cxx
+++ src/Cxx/ImplicitFunctions/IsoContours.cxx
@@ -1,5 +1,6 @@
-#include <vtkVersion.h>
 #include <vtkSmartPointer.h>
+#include <vtkContourFilter.h>
+
 #include <vtkInteractorStyleUser.h>
 #include <vtkProperty.h>
 #include <vtkOutlineFilter.h>
@@ -16,10 +17,13 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
-#include <vtkContourFilter.h>
 #include <vtkXMLImageDataWriter.h>
+#include <vtkNamedColors.h>
 
-static void CreateData(vtkImageData* data);
+
+namespace
+{
+void CreateData(vtkImageData* data);
 
 class vtkSliderCallback : public vtkCommand
 {
@@ -39,9 +43,13 @@ public:
   vtkSliderCallback():ContourFilter(NULL) {}
   vtkContourFilter* ContourFilter;
 };
+}
 
 int main(int, char *[])
 {
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+
   vtkSmartPointer<vtkImageData> data =
     vtkSmartPointer<vtkImageData>::New();
   CreateData(data);
@@ -49,11 +57,7 @@ int main(int, char *[])
   // Create an isosurface
   vtkSmartPointer<vtkContourFilter> contourFilter =
     vtkSmartPointer<vtkContourFilter>::New();
-#if VTK_MAJOR_VERSION <= 5
-  contourFilter->SetInput(data);
-#else
   contourFilter->SetInputData(data);
-#endif
   contourFilter->GenerateValues(1, 10, 10); // (numContours, rangeStart, rangeEnd)
 
   // Map the contours to graphical primitives
@@ -65,21 +69,21 @@ int main(int, char *[])
   vtkSmartPointer<vtkActor> contourActor =
     vtkSmartPointer<vtkActor>::New();
   contourActor->SetMapper(contourMapper);
+  contourActor->GetProperty()->SetLineWidth(5);
 
   // Create the outline
   vtkSmartPointer<vtkOutlineFilter> outlineFilter =
     vtkSmartPointer<vtkOutlineFilter>::New();
-#if VTK_MAJOR_VERSION <= 5
-  outlineFilter->SetInput(data);
-#else
   outlineFilter->SetInputData(data);
-#endif
+
   vtkSmartPointer<vtkPolyDataMapper> outlineMapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
   outlineMapper->SetInputConnection(outlineFilter->GetOutputPort());
   vtkSmartPointer<vtkActor> outlineActor =
     vtkSmartPointer<vtkActor>::New();
   outlineActor->SetMapper(outlineMapper);
+  outlineActor->GetProperty()->SetColor(colors->GetColor3d("Gray").GetData());
+  outlineActor->GetProperty()->SetLineWidth(3);
 
   // Visualize
   vtkSmartPointer<vtkRenderer> renderer =
@@ -93,6 +97,7 @@ int main(int, char *[])
 
   renderer->AddActor(contourActor);
   renderer->AddActor(outlineActor);
+  renderer->SetBackground(colors->GetColor3d("Burlywood").GetData());
 
   vtkSmartPointer<vtkSliderRepresentation3D> sliderRep =
     vtkSmartPointer<vtkSliderRepresentation3D>::New();
@@ -129,16 +134,12 @@ int main(int, char *[])
 
   return EXIT_SUCCESS;
 }
-
+namespace
+{
 void CreateData(vtkImageData* data)
 {
   data->SetExtent(-25,25,-25,25,0,0);
-#if VTK_MAJOR_VERSION <= 5
-  data->SetNumberOfScalarComponents(1);
-  data->SetScalarTypeToDouble();
-#else
   data->AllocateScalars(VTK_DOUBLE,1);
-#endif
   int* extent = data->GetExtent();
 
   for (int y = extent[2]; y <= extent[3]; y++)
@@ -153,10 +154,7 @@ void CreateData(vtkImageData* data)
   vtkSmartPointer<vtkXMLImageDataWriter> writer =
     vtkSmartPointer<vtkXMLImageDataWriter>::New();
   writer->SetFileName("data.vti");
-#if VTK_MAJOR_VERSION <= 5
-  writer->SetInputConnection(data->GetProducerPort());
-#else
   writer->SetInputData(data);
-#endif
   writer->Write();
+}
 }
