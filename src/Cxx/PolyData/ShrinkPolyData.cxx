cbc8d1face0e975fc78923f7821ff110668ebbb0
Bill Lorensen <bill.lorensen@gmail.com>
2017-08-04 11:21:14 -0400

    ENH: Improved ShrinkPolyData example

--- src/Cxx/PolyData/ShrinkPolyData.cxx
+++ src/Cxx/PolyData/ShrinkPolyData.cxx
@@ -1,36 +1,87 @@
 #include <vtkSmartPointer.h>
-#include <vtkPolyData.h>
-#include <vtkSphereSource.h>
+
 #include <vtkShrinkPolyData.h>
-#include <vtkXMLPolyDataWriter.h>
+#include <vtkSphereSource.h>
 
-int main(int, char *[])
+#include <vtkPolyData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkXMLPolyDataReader.h>
+
+#include <vtkNamedColors.h>
+
+int main(int argc, char *argv[])
 {
-  vtkSmartPointer<vtkSphereSource> sphereSource = 
-      vtkSmartPointer<vtkSphereSource>::New();
-  sphereSource->SetRadius(10);
-  sphereSource->SetPhiResolution(50);
-  sphereSource->SetThetaResolution(50);
-  sphereSource->Update();
-
+  vtkSmartPointer<vtkPolyData> inputPolyData;
+  if(argc > 1)
   {
-  vtkSmartPointer<vtkXMLPolyDataWriter> writer = 
-    vtkSmartPointer<vtkXMLPolyDataWriter>::New();
-  writer->SetFileName("sphere.vtp");
-  writer->SetInputConnection(sphereSource->GetOutputPort());
-  writer->Write();
+    vtkSmartPointer<vtkXMLPolyDataReader> reader =
+      vtkSmartPointer<vtkXMLPolyDataReader>::New();
+    reader->SetFileName(argv[1]);
+    reader->Update();
+    inputPolyData = reader->GetOutput();
+  }
+  else
+  {
+    vtkSmartPointer<vtkSphereSource> sphereSource =
+      vtkSmartPointer<vtkSphereSource>::New();
+    sphereSource->SetPhiResolution(15);
+    sphereSource->SetThetaResolution(15);
+    sphereSource->Update();
+    inputPolyData = sphereSource->GetOutput();
   }
 
-  vtkSmartPointer<vtkShrinkPolyData> shrinkFilter =
-      vtkSmartPointer<vtkShrinkPolyData>::New();
-  shrinkFilter->SetInputConnection(sphereSource->GetOutputPort());
-  shrinkFilter->Update();
-  
-  vtkSmartPointer<vtkXMLPolyDataWriter> writer = 
-      vtkSmartPointer<vtkXMLPolyDataWriter>::New();
-  writer->SetFileName("output.vtp");
-  writer->SetInputConnection(shrinkFilter->GetOutputPort());
-  writer->Write();
-  
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+
+  vtkSmartPointer<vtkShrinkPolyData> shrink =
+    vtkSmartPointer<vtkShrinkPolyData>::New();
+  shrink->SetShrinkFactor(.8);
+  shrink->SetInputData(inputPolyData);
+
+  // Visualize
+  vtkSmartPointer<vtkRenderer> renderer =
+    vtkSmartPointer<vtkRenderer>::New();
+
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+
+  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  interactor->SetRenderWindow(renderWindow);
+
+  vtkSmartPointer<vtkPolyDataMapper> mapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper->SetInputConnection(shrink->GetOutputPort());
+  mapper->ScalarVisibilityOff();
+
+  vtkSmartPointer<vtkProperty> back =
+    vtkSmartPointer<vtkProperty>::New();
+  back->SetColor(colors->GetColor3d("Peacock").GetData());
+
+  vtkSmartPointer<vtkActor> actor =
+    vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
+  actor->GetProperty()->SetColor(colors->GetColor3d("Salmon").GetData());
+  actor->SetBackfaceProperty(back);
+
+  renderer->AddActor(actor);
+  renderer->SetBackground(colors->GetColor3d("Burlywood").GetData());
+  renderer->ResetCamera();
+  renderer->GetActiveCamera()->Azimuth(30);
+  renderer->GetActiveCamera()->Elevation(30);
+  renderer->GetActiveCamera()->Dolly(1.5);;
+  renderer->ResetCameraClippingRange();
+
+  renderWindow->SetSize(640, 480);
+  renderWindow->Render();
+  interactor->Start();
+
   return EXIT_SUCCESS;
 }
