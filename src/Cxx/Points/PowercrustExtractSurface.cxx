9a97c3cea2a4f2da0be39da6b7ff6cccd09dab3c
Bill Lorensen <bill.lorensen@gmail.com>
2017-09-05 16:15:00 -0400

    ENH: New PowercrustExtractSurface example

---
+++ src/Cxx/Points/PowercrustExtractSurface.cxx
@@ -0,0 +1,137 @@
+#include <vtkSmartPointer.h>
+
+#include <vtkPLYReader.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkOBJReader.h>
+#include <vtkSTLReader.h>
+#include <vtkPointSource.h>
+
+#include <vtkPowerCrustSurfaceReconstruction.h>
+
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkCamera.h>
+
+#include <vtkNamedColors.h>
+#include <vtksys/SystemTools.hxx>
+
+namespace
+{
+vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName);
+}
+
+int main (int argc, char *argv[])
+{
+  vtkSmartPointer<vtkPolyData> polyData = ReadPolyData(argc > 1 ? argv[1] : "");;
+
+  vtkSmartPointer<vtkPowerCrustSurfaceReconstruction> surface =
+    vtkSmartPointer<vtkPowerCrustSurfaceReconstruction>::New();
+  surface->SetInputData (polyData);
+
+  vtkSmartPointer<vtkPolyDataMapper> surfaceMapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  surfaceMapper->SetInputConnection(surface->GetOutputPort());
+
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+
+  vtkSmartPointer<vtkProperty> back =
+    vtkSmartPointer<vtkProperty>::New();
+  back->SetColor(colors->GetColor3d("banana").GetData());
+    
+  vtkSmartPointer<vtkActor> surfaceActor =
+    vtkSmartPointer<vtkActor>::New();
+  surfaceActor->SetMapper(surfaceMapper);
+  surfaceActor->GetProperty()->SetColor(colors->GetColor3d("Tomato").GetData());
+  surfaceActor->SetBackfaceProperty(back);
+
+  // Create graphics stuff
+  //
+  vtkSmartPointer<vtkRenderer> ren1 =
+    vtkSmartPointer<vtkRenderer>::New();
+  ren1->SetBackground(colors->GetColor3d("SlateGray").GetData());
+
+  vtkSmartPointer<vtkRenderWindow> renWin =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renWin->AddRenderer(ren1);
+  renWin->SetSize(512,512);
+
+  vtkSmartPointer<vtkRenderWindowInteractor> iren =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  iren->SetRenderWindow(renWin);
+
+  // Add the actors to the renderer, set the background and size
+  //
+  ren1->AddActor(surfaceActor);
+
+  // Generate an interesting view
+  //
+  ren1->ResetCamera();
+  ren1->GetActiveCamera()->Azimuth(120);
+  ren1->GetActiveCamera()->Elevation(30);
+  ren1->GetActiveCamera()->Dolly(1.0);
+  ren1->ResetCameraClippingRange();
+
+  iren->Initialize();
+  iren->Start();
+
+  return EXIT_SUCCESS;
+}
+
+namespace
+{
+vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName)
+{
+  vtkSmartPointer<vtkPolyData> polyData;
+  std::string extension = vtksys::SystemTools::GetFilenameExtension(std::string(fileName));
+  if (extension == ".ply")
+  {
+    vtkSmartPointer<vtkPLYReader> reader =
+      vtkSmartPointer<vtkPLYReader>::New();
+    reader->SetFileName (fileName);
+    reader->Update();
+    polyData = reader->GetOutput();
+  }
+  else if (extension == ".vtp")
+  {
+    vtkSmartPointer<vtkXMLPolyDataReader> reader =
+      vtkSmartPointer<vtkXMLPolyDataReader>::New();
+    reader->SetFileName (fileName);
+    reader->Update();
+    polyData = reader->GetOutput();
+  }
+  else if (extension == ".obj")
+  {
+    vtkSmartPointer<vtkOBJReader> reader =
+      vtkSmartPointer<vtkOBJReader>::New();
+    reader->SetFileName (fileName);
+    reader->Update();
+    polyData = reader->GetOutput();
+  }
+  else if (extension == ".stl")
+  {
+    vtkSmartPointer<vtkSTLReader> reader =
+      vtkSmartPointer<vtkSTLReader>::New();
+    reader->SetFileName (fileName);
+    reader->Update();
+    polyData = reader->GetOutput();
+  }
+  else
+  {
+    vtkSmartPointer<vtkPointSource> points =
+      vtkSmartPointer<vtkPointSource>::New();
+    points->SetNumberOfPoints(1000);
+    points->SetRadius(1.0);
+    points->SetCenter(vtkMath::Random(-1, 1),
+                      vtkMath::Random(-1, 1),
+                      vtkMath::Random(-1, 1));
+    points->SetDistributionToShell();
+    points->Update();
+    polyData = points->GetOutput();
+  }
+  return polyData;
+}
+}
