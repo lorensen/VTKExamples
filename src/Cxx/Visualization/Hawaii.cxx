ddef8a5678897256c524c2869eb3ab01a0963f56
Bill Lorensen <bill.lorensen@gmail.com>
2017-10-16 17:05:00 -0700

    DOC: Remove non-utf8 characters

--- src/Cxx/Visualization/Hawaii.cxx
+++ src/Cxx/Visualization/Hawaii.cxx
@@ -2,7 +2,7 @@
 
 /*
 
-Produce figure 6–12 from the VTK Textbook.
+Produce figure 6-12 from the VTK Textbook.
 It is a translation of the original hawaii.tcl with a few additional
 enhancements.
 The image is centered on Honolulu, O'ahu.
