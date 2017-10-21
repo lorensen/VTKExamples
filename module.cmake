f939ef43caf9e4e844c1e0c7181eb8b54737c22e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-10-21 05:46:05 -0400

    module.cmake: Update list based on output of WhatModulesVTK.py

--- module.cmake
+++ module.cmake
@@ -1,44 +1,66 @@
 set(DOCUMENTATION "A collection of examples illustrating how to use VTK")
 
-set(qt_modules "")
-if (Module_vtkGUISupportQtOpenGL)
-  set(qt_modules vtkRenderingQt vtkViewsQt vtkGUISupportQtOpenGL)
+set(DEPENDS
+  vtkChartsCore
+  vtkCommonColor
+  vtkCommonComputationalGeometry
+  vtkCommonSystem
+  vtkFiltersExtraction
+  vtkFiltersFlowPaths
+  vtkFiltersGeneric
+  vtkFiltersGeometry
+  vtkFiltersHybrid
+  vtkFiltersModeling
+  # vtkFiltersParallelStatistics #  Optional
+  vtkFiltersPoints
+  vtkFiltersProgrammable
+  vtkFiltersTexture
+  vtkFiltersVerdict
+  vtkGeovisCore
+  vtkIOExodus
+  vtkIOExport
+  # vtkIOFFMPEG # Optional
+  vtkIOImport
+  vtkIOInfovis
+  # vtkIOMySQL # Optional
+  vtkIOPLY
+  vtkIOParallel
+  vtkIOParallelXML
+  vtkImagingColor
+  vtkImagingFourier
+  vtkImagingHybrid
+  vtkImagingMath
+  vtkImagingMorphological
+  vtkImagingSources
+  vtkImagingStatistics
+  vtkImagingStencil
+  # vtkInfovisBoostGraphAlgorithms # Optional
+  vtkInfovisLayout
+  vtkInteractionImage
+  vtkRenderingAnnotation
+  vtkRenderingContext${VTK_RENDERING_BACKEND}
+  vtkRenderingImage
+  vtkRenderingLOD
+  vtkRenderingLabel
+  vtkRendering${VTK_RENDERING_BACKEND}
+  vtkRenderingVolume${VTK_RENDERING_BACKEND}
+  vtkTestingGenericBridge
+  vtkTestingRendering
+  vtkViewsContext2D
+  vtkViewsInfovis
+  )
+
+if(Module_vtkGUISupportQtOpenGL)
+  list(APPEND DEPENDS
+    vtkGUISupportQtOpenGL
+    vtkRenderingQt
+    vtkViewsQt
+    )
 endif()
 
 vtk_module( WikiExamples
   DEPENDS
-  vtkFiltersFlowPaths
-  vtkFiltersGeneric
-  vtkFiltersGeometry
-  vtkFiltersParallelStatistics
-  vtkFiltersPoints
-  vtkFiltersProgrammable
-  vtkFiltersSelection
-  vtkFiltersTexture
-  vtkFiltersVerdict
-  vtkIOExodus
-  vtkIOExport
-  vtkIOImport
-  vtkIOInfovis
-  vtkIOLSDyna
-  vtkIOPLY
-  vtkIOParallel
-  vtkIOParallelXML
-  vtkImagingMath
-  vtkImagingMorphological
-  vtkImagingStatistics
-  vtkImagingStencil
-  vtkInteractionImage
-  vtkRenderingContext${VTK_RENDERING_BACKEND}
-  vtkRenderingImage
-  vtkRenderingLOD
-  vtkRenderingVolume${VTK_RENDERING_BACKEND}
-  vtkRendering${VTK_RENDERING_BACKEND}
-  vtkTestingGenericBridge
-  vtkTestingRendering
-  vtkViewsContext2D
-  vtkViewsGeovis
-  ${qt_modules}
+    ${DEPENDS}
   DESCRIPTION
     "${DOCUMENTATION}"
 )
