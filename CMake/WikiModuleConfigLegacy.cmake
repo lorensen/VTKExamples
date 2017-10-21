4fc824a76067b2b658bd03bbb848c77b69da940f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-10-21 03:54:12 -0400

    STYLE: Simplify top-level CMakeLists introducing WikiModuleConfigLegacy
    The module has the "Legacy" prefix anticipating a module named
    WikiModuleConfig will be added.

---
+++ CMake/WikiModuleConfigLegacy.cmake
@@ -0,0 +1,40 @@
+
+#
+# This CMake module implements the legacy logic that is used
+# to find and load the VTK dependencies needed by this project.
+#
+
+#
+# It set thes variable `WikiModuleConfigLegacy_RETURN` to 1 in the
+# including scope to indicate that "return()" should be called.
+#
+
+get_filename_component(_name ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
+message(STATUS "VTKWikiExamples:")
+message(STATUS "VTKWikiExamples: Including VTK using: ${_name}")
+message(STATUS "VTKWikiExamples:")
+
+set(${_name}_RETURN 0)
+
+if(Module_WikiExamples)
+  #
+  # This is a VTK remote module build
+  #
+
+  vtk_module_impl()
+  if(NOT EXISTS "${VTK_BINARY_DIR}/VTKConfig.cmake")
+    set(${_name}_RETURN 1)
+    return()
+  endif()
+
+  find_package(VTK PATHS ${VTK_BINARY_DIR})
+  include(${VTK_USE_FILE})
+else()
+  #
+  # This is a stand-alone build
+  #
+
+  find_package(VTK REQUIRED)
+  include(${VTK_USE_FILE})
+endif()
+
