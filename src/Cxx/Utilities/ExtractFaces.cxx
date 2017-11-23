59efb78b9f10e6cde11c84c4221c32751ebe693f
Bill Lorensen <bill.lorensen@gmail.com>
2017-11-23 06:54:45 -0500

    BUG: Fixed memory leak

--- src/Cxx/Utilities/ExtractFaces.cxx
+++ src/Cxx/Utilities/ExtractFaces.cxx
@@ -29,15 +29,15 @@ int main (int argc, char *argv[])
       std::string cellName = vtkCellTypes::GetClassNameFromTypeId(cell->GetCellType());
       if (cellMap.count(cellName) == 0)
       {
-        int numFaces = cell->GetNumberOfFaces();
         std::cout << "Type: " << cellName
                   << " has " << cell->GetNumberOfFaces() << " faces" << std::endl;
       }
       cellMap[cellName]++;
     }
-}
-  std::map <std::string, int>::iterator itm = cellMap.begin();
+    it->Delete();
+  }
 
+  std::map <std::string, int>::iterator itm = cellMap.begin();
   for (itm = cellMap.begin(); itm != cellMap.end(); ++itm)
   {
     std::cout << itm->first << " occurs " << itm->second << " times" << std::endl;
