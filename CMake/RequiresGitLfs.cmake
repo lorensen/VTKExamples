07454021bf0b71205c0378b26aebead8af403013
Bill Lorensen <bill.lorensen@gmail.com>
2017-11-30 12:06:16 -0500

    ENH: Trying to enable git lfs

--- CMake/RequiresGitLfs.cmake
+++ CMake/RequiresGitLfs.cmake
@@ -1,7 +1,7 @@
 macro(Requires_GitLfs example)
   file(READ "${WikiExamples_SOURCE_DIR}/src/Testing/Data/GitLfsInstalled" data)
   message(STATUS "data|${data}|")
-  if (NOT data STREQUAL "\\n")
+  if (data STREQUAL "GitLfsNotInstalled")
     message(STATUS "VTKWikiExamples: ${example} requires git lfs to be installed and will not be built")
     string(REGEX REPLACE "[^;]*${example}.cxx"
            "" ALL_FILES "${ALL_FILES}")
