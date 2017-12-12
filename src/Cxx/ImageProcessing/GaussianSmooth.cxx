400d1c6feefbbc447a24a5ea876b23ddbbf4afca
Bill Lorensen <bill.lorensen@gmail.com>
2017-12-12 08:49:47 -0500

    ENH: Added GaussianSmooth and MedianComparison

---
+++ src/Cxx/ImageProcessing/GaussianSmooth.cxx
@@ -0,0 +1,103 @@
+#include <vtkSmartPointer.h>
+#include <vtkImageGaussianSmooth.h>
+#include <vtkImageReader2Factory.h>
+#include <vtkImageReader2.h>
+
+#include <vtkImageData.h>
+#include <vtkImageMapper3D.h>
+#include <vtkImageNoiseSource.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleImage.h>
+#include <vtkRenderer.h>
+#include <vtkImageActor.h>
+#include <vtkImageCast.h>
+#include <vtkNamedColors.h>
+#include <vtkInteractorStyleImage.h>
+
+int main( int argc, char *argv[])
+{
+  // Verify input arguments
+  if ( argc != 2 )
+  {
+    std::cout << "Usage: " << argv[0]
+              << " Filename" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Read the image
+  vtkSmartPointer<vtkImageReader2Factory> readerFactory =
+    vtkSmartPointer<vtkImageReader2Factory>::New();
+  vtkImageReader2 *reader = readerFactory->CreateImageReader2(argv[1]);
+  reader->SetFileName(argv[1]);
+  reader->Update();
+
+  // Process the image
+  vtkSmartPointer<vtkImageCast> cast =
+    vtkSmartPointer<vtkImageCast>::New();
+  cast->SetInputConnection(reader->GetOutputPort());
+  cast->SetOutputScalarTypeToFloat();
+
+  vtkSmartPointer<vtkImageGaussianSmooth> filter =
+    vtkSmartPointer<vtkImageGaussianSmooth>::New();
+  filter->SetDimensionality(2);
+  filter->SetInputConnection(cast->GetOutputPort());
+  filter->SetStandardDeviations(4.0, 4.0);
+  filter->SetRadiusFactors(2.0, 2.0);
+                           
+  // Create actors
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+
+  vtkSmartPointer<vtkImageActor> originalActor =
+    vtkSmartPointer<vtkImageActor>::New();
+  originalActor->GetMapper()->SetInputConnection(
+    reader->GetOutputPort());
+
+  vtkSmartPointer<vtkImageActor> filteredActor =
+    vtkSmartPointer<vtkImageActor>::New();
+  filteredActor->GetMapper()->SetInputConnection(
+    filter->GetOutputPort());
+
+  // Define viewport ranges
+  // (xmin, ymin, xmax, ymax)
+  double originalViewport[4] = {0.0, 0.0, 0.5, 1.0};
+  double filteredViewport[4] = {0.5, 0.0, 1.0, 1.0};
+
+  // Setup renderers
+  vtkSmartPointer<vtkRenderer> originalRenderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  originalRenderer->SetViewport(originalViewport);
+  originalRenderer->AddActor(originalActor);
+  originalRenderer->ResetCamera();
+  originalRenderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
+
+  vtkSmartPointer<vtkRenderer> filteredRenderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  filteredRenderer->SetViewport(filteredViewport);
+  filteredRenderer->AddActor(filteredActor);
+  filteredRenderer->ResetCamera();
+  filteredRenderer->SetBackground(colors->GetColor3d("LightSlateGray").GetData());
+
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->SetSize(600, 300);
+  renderWindow->AddRenderer(originalRenderer);
+  renderWindow->AddRenderer(filteredRenderer);
+
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  vtkSmartPointer<vtkInteractorStyleImage> style =
+    vtkSmartPointer<vtkInteractorStyleImage>::New();
+
+  renderWindowInteractor->SetInteractorStyle(style);
+
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+  renderWindowInteractor->Initialize();
+
+  renderWindowInteractor->Start();
+
+  reader->Delete();
+
+  return EXIT_SUCCESS;
+}
