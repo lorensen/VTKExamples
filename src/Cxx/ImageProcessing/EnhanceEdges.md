3c31948b562ed9946df01d67e662efb21fcf2258
Bill Lorensen <bill.lorensen@gmail.com>
2017-12-15 07:45:23 -0500

    ENH: New example VTKSpectrum

---
+++ src/Cxx/ImageProcessing/EnhanceEdges.md
@@ -0,0 +1,4 @@
+### Description
+High-pass filters can also be used to compress the range of an image. Since low frequencies account for much of the dynamic range of an image but carry little information, a high-pass filter can significantly decrease an image’s scalar range and emphasize hidden details. The Laplacian filter, which is a second derivative operation, is one implementation of a high-pass filter. It eliminates constant and low frequencies leaving only high-frequency edges. The output of the Laplacian can be subtracted from the original image to produce edge enhancement or sharpening of an image.
+
+This example subtracts the Laplacian (middle) from the original image (left) resulting in edge enhancement or a sharpening operation (right).
---
+++ src/Cxx/ImageProcessing/VTKSpectrum.cxx
@@ -0,0 +1,145 @@
+#include <vtkSmartPointer.h>
+
+#include <vtkImageReader2Factory.h>
+#include <vtkImageReader2.h>
+
+#include <vtkImageFFT.h>
+#include <vtkImageFourierCenter.h>
+#include <vtkImageLogarithmicScale.h>
+#include <vtkImageMagnitude.h>
+
+#include <vtkWindowLevelLookupTable.h>
+#include <vtkImageMapToColors.h>
+
+#include <vtkImageData.h>
+#include <vtkInteractorStyleImage.h>
+#include <vtkImageMapper3D.h>
+#include <vtkImageProperty.h>
+#include <vtkImageActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkNamedColors.h>
+
+namespace
+{
+void CreateImageActor(vtkSmartPointer<vtkImageActor> &actor,
+                      double colorWindow, double colorLevel);
+}
+ int main (int argc, char *argv[])
+{
+  // Verify input arguments
+  if ( argc != 2 )
+  {
+    std::cout << "Usage: " << argv[0]
+              << " Filename" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Read the image
+  vtkSmartPointer<vtkImageReader2Factory> readerFactory =
+    vtkSmartPointer<vtkImageReader2Factory>::New();
+  vtkImageReader2 *reader = readerFactory->CreateImageReader2(argv[1]);
+  reader->SetFileName(argv[1]);
+  reader->Update();
+
+
+  vtkSmartPointer<vtkImageFFT> fft =
+    vtkSmartPointer<vtkImageFFT>::New();
+//  fft->SetFilteredAxes $VTK_IMAGE_X_AXIS $VTK_IMAGE_Y_AXIS
+  fft->SetInputConnection(reader->GetOutputPort());
+
+  vtkSmartPointer<vtkImageMagnitude> mag =
+    vtkSmartPointer<vtkImageMagnitude>::New();
+  mag->SetInputConnection(fft->GetOutputPort());
+
+  vtkSmartPointer<vtkImageFourierCenter> center =
+    vtkSmartPointer<vtkImageFourierCenter>::New();
+  center->SetInputConnection(mag->GetOutputPort());
+ 
+  vtkSmartPointer<vtkImageLogarithmicScale> compress =
+    vtkSmartPointer<vtkImageLogarithmicScale>::New();
+  compress->SetInputConnection(center->GetOutputPort());
+  compress->SetConstant(15);
+  compress->Update();
+
+  // Create actors
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+
+  vtkSmartPointer<vtkImageActor> originalActor =
+    vtkSmartPointer<vtkImageActor>::New();
+  originalActor->GetMapper()->SetInputConnection(reader->GetOutputPort());
+  originalActor->GetProperty()->SetInterpolationTypeToNearest();
+
+  vtkSmartPointer<vtkImageActor> compressedActor =
+    vtkSmartPointer<vtkImageActor>::New();
+  compressedActor->GetMapper()->SetInputConnection(compress->GetOutputPort());
+  compressedActor->GetProperty()->SetInterpolationTypeToNearest();
+  CreateImageActor(compressedActor, 160, 120);
+
+  // Define viewport ranges
+  // (xmin, ymin, xmax, ymax)
+  double originalViewport[4] = {0.0, 0.0, 0.5, 1.0};
+  double compressedViewport[4] = {0.5, 0.0, 1.0, 1.0};
+
+  // Setup renderers
+  vtkSmartPointer<vtkRenderer> originalRenderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  originalRenderer->SetViewport(originalViewport);
+  originalRenderer->AddActor(originalActor);
+  originalRenderer->ResetCamera();
+  originalRenderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
+
+  vtkSmartPointer<vtkRenderer> compressedRenderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  compressedRenderer->SetViewport(compressedViewport);
+  compressedRenderer->AddActor(compressedActor);
+  compressedRenderer->ResetCamera();
+  compressedRenderer->SetBackground(colors->GetColor3d("LightSlateGray").GetData());
+
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->SetSize(600, 300);
+  renderWindow->AddRenderer(originalRenderer);
+  renderWindow->AddRenderer(compressedRenderer);
+
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  vtkSmartPointer<vtkInteractorStyleImage> style =
+    vtkSmartPointer<vtkInteractorStyleImage>::New();
+
+  renderWindowInteractor->SetInteractorStyle(style);
+
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+  renderWindowInteractor->Initialize();
+
+  renderWindowInteractor->Start();
+
+  reader->Delete();
+  return EXIT_SUCCESS;
+}
+
+namespace
+{
+void CreateImageActor(vtkSmartPointer<vtkImageActor> &actor,
+                      double colorWindow, double colorLevel)
+{
+  vtkSmartPointer<vtkWindowLevelLookupTable> wlut =
+    vtkSmartPointer<vtkWindowLevelLookupTable>::New();
+  wlut->SetWindow(colorWindow);
+  wlut->SetLevel(colorLevel);
+  wlut->Build();
+
+  vtkImageData *imageData =
+    vtkImageData::SafeDownCast(actor->GetMapper()->GetInput());
+  // Map the image through the lookup table
+  vtkSmartPointer<vtkImageMapToColors> color =
+    vtkSmartPointer<vtkImageMapToColors>::New();
+  color->SetLookupTable(wlut);
+  color->SetInputData(actor->GetMapper()->GetInput());
+
+  actor->GetMapper()->SetInputConnection(color->GetOutputPort());
+  return;
+}
+}
