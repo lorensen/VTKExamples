b0d9f2e9508bd773da1cae3b63202335d2847ee3
Bill Lorensen <bill.lorensen@gmail.com>
2017-12-25 07:19:30 -0500

    ENH: Two new examples from VTK Book.

---
+++ src/Cxx/Rendering/ColoredSphere.cxx
@@ -0,0 +1,54 @@
+#include <vtkSmartPointer.h>
+
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkSphereSource.h>
+#include <vtkElevationFilter.h>
+#include <vtkDataSetMapper.h>
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkNamedColors.h>
+
+int main( int, char *[] )
+{
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+  vtkSmartPointer<vtkRenderer> renderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renWin =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renWin->AddRenderer(renderer);
+  vtkSmartPointer<vtkRenderWindowInteractor> iren =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  iren->SetRenderWindow(renWin);
+
+  vtkSmartPointer<vtkSphereSource> sphere =
+    vtkSmartPointer<vtkSphereSource>::New();
+  sphere->SetPhiResolution(12); sphere->SetThetaResolution(12);
+
+  vtkSmartPointer<vtkElevationFilter> colorIt =
+    vtkSmartPointer<vtkElevationFilter>::New();
+  colorIt->SetInputConnection(sphere->GetOutputPort());
+  colorIt->SetLowPoint(0,0,-1);
+  colorIt->SetHighPoint(0,0,1);
+
+  vtkSmartPointer<vtkDataSetMapper> mapper =
+    vtkSmartPointer<vtkDataSetMapper>::New();
+  mapper->SetInputConnection(colorIt->GetOutputPort());
+
+  vtkSmartPointer<vtkActor> actor =
+    vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
+
+  renderer->AddActor(actor);
+  renderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
+  renWin->SetSize(640, 480);
+
+  renWin->Render();
+
+  // interact with data
+  iren->Start();
+  
+  return EXIT_SUCCESS;
+}
---
+++ src/Cxx/Rendering/TransformSphere.cxx
@@ -0,0 +1,76 @@
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkSphereSource.h>
+#include <vtkTransform.h>
+#include <vtkTransformFilter.h>
+#include <vtkElevationFilter.h>
+#include <vtkLookupTable.h>
+#include <vtkDataSetMapper.h>
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkNamedColors.h>
+
+int main(int, char *[])
+{
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+  vtkSmartPointer<vtkRenderer> renderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renWin =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renWin->AddRenderer(renderer);
+  vtkSmartPointer<vtkRenderWindowInteractor> iren =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  iren->SetRenderWindow(renWin);
+
+  vtkSmartPointer<vtkSphereSource> sphere =
+    vtkSmartPointer<vtkSphereSource>::New();
+  sphere->SetThetaResolution(12); sphere->SetPhiResolution(12);
+
+  vtkSmartPointer<vtkTransform> aTransform =
+    vtkSmartPointer<vtkTransform>::New();
+  aTransform->Scale(1,1.5,2);
+
+  vtkSmartPointer<vtkTransformFilter> transFilter =
+    vtkSmartPointer<vtkTransformFilter>::New();
+  transFilter->SetInputConnection(sphere->GetOutputPort());
+  transFilter->SetTransform(aTransform);  
+
+  vtkSmartPointer<vtkElevationFilter> colorIt =
+    vtkSmartPointer<vtkElevationFilter>::New();
+  colorIt->SetInputConnection(transFilter->GetOutputPort());
+  colorIt->SetLowPoint(0,0,-1);
+  colorIt->SetHighPoint(0,0,1);
+
+  vtkSmartPointer<vtkLookupTable> lut =
+    vtkSmartPointer<vtkLookupTable>::New();
+  lut->SetHueRange(0.667, 0);
+  lut->SetSaturationRange(1,1);
+  lut->SetValueRange(1,1);
+
+  vtkSmartPointer<vtkDataSetMapper> mapper =
+    vtkSmartPointer<vtkDataSetMapper>::New();
+  mapper->SetLookupTable(lut);
+  mapper->SetInputConnection(colorIt->GetOutputPort());
+
+  vtkSmartPointer<vtkActor> actor =
+    vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
+
+  renderer->AddActor(actor);
+  renderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
+  renderer->ResetCamera();
+  renderer->GetActiveCamera()->Elevation(60.0);
+  renderer->GetActiveCamera()->Azimuth(30.0);
+  renderer->ResetCameraClippingRange();
+
+  renWin->SetSize(640, 480);
+
+  renWin->Render();
+
+  // interact with data
+  iren->Start();
+
+  return EXIT_SUCCESS;
+}
