c98e3debdc40513fd45ae615e20dc35a8a9a2220
Andrew J. P. Maclean <andrew.amaclean@gmail.com>
2018-02-24 14:46:28 +1100

    Updating Arrow.

--- src/Python/GeometricObjects/Arrow.py
+++ src/Python/GeometricObjects/Arrow.py
@@ -1,26 +1,37 @@
-# arrow.py adapted from the C++ vtk examples and translated to python.
+#!/usr/bin/env python
 
 import vtk
 
-arrowSource = vtk.vtkArrowSource()
-# arrowSource.SetShaftRadius(0.01)
-# arrowSource.SetTipLength(.9)
 
-# Create a mapper and actor
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(arrowSource.GetOutputPort())
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
+# arrow.py adapted from the C++ vtk examples and translated to python.
 
-# Visualize
-renderer = vtk.vtkRenderer()
-renderWindow = vtk.vtkRenderWindow()
-renderWindow.AddRenderer(renderer)
-renderWindowInteractor = vtk.vtkRenderWindowInteractor()
-renderWindowInteractor.SetRenderWindow(renderWindow)
+def main():
+    colors = vtk.vtkNamedColors()
 
-renderer.AddActor(actor)
-renderer.SetBackground(.1, .2, .3)  # Background color dark blue
+    arrowSource = vtk.vtkArrowSource()
+    # arrowSource.SetShaftRadius(0.01)
+    # arrowSource.SetTipLength(.9)
 
-renderWindow.Render()
-renderWindowInteractor.Start()
+    # Create a mapper and actor
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(arrowSource.GetOutputPort())
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+
+    # Visualize
+    renderer = vtk.vtkRenderer()
+    renderWindow = vtk.vtkRenderWindow()
+    renderWindow.SetWindowName("Arrow")
+    renderWindow.AddRenderer(renderer)
+    renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+    renderWindowInteractor.SetRenderWindow(renderWindow)
+
+    renderer.AddActor(actor)
+    renderer.SetBackground(colors.GetColor3d("MidnightBlue"))
+
+    renderWindow.Render()
+    renderWindowInteractor.Start()
+
+
+if __name__ == "__main__":
+    main()
