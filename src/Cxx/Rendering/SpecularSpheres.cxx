a82a92d677294f5bcf0dc180291b0df08b4bd093
Bill Lorensen <bill.lorensen@gmail.com>
2018-04-02 12:45:44 -0700

    ENH: Example better matches text book figure

--- src/Cxx/Rendering/SpecularSpheres.cxx
+++ src/Cxx/Rendering/SpecularSpheres.cxx
@@ -47,71 +47,79 @@ int main(int, char* [])
   sphere1->SetMapper(sphereMapper);
   sphere1->GetProperty()->SetColor(1, 0, 0);
   sphere1->GetProperty()->SetAmbient(0.3);
-  sphere1->GetProperty()->SetDiffuse(0.0);
+  sphere1->GetProperty()->SetDiffuse(0.5);
   sphere1->GetProperty()->SetSpecular(1.0);
+  sphere1->GetProperty()->SetSpecularColor(1, 1, 1);
   sphere1->GetProperty()->SetSpecularPower(5.0);
 
   vtkSmartPointer<vtkActor> sphere2 = vtkSmartPointer<vtkActor>::New();
   sphere2->SetMapper(sphereMapper);
   sphere2->GetProperty()->SetColor(1, 0, 0);
   sphere2->GetProperty()->SetAmbient(0.3);
-  sphere2->GetProperty()->SetDiffuse(0.0);
+  sphere2->GetProperty()->SetDiffuse(0.5);
   sphere2->GetProperty()->SetSpecular(1.0);
   sphere2->GetProperty()->SetSpecularPower(10.0);
+  sphere2->GetProperty()->SetSpecularColor(1, 1, 1);
   sphere2->AddPosition(1.25, 0, 0);
 
   vtkSmartPointer<vtkActor> sphere3 = vtkSmartPointer<vtkActor>::New();
   sphere3->SetMapper(sphereMapper);
   sphere3->GetProperty()->SetColor(1, 0, 0);
   sphere3->GetProperty()->SetAmbient(0.3);
-  sphere3->GetProperty()->SetDiffuse(0.0);
+  sphere3->GetProperty()->SetDiffuse(.5);
   sphere3->GetProperty()->SetSpecular(1.0);
   sphere3->GetProperty()->SetSpecularPower(20.0);
+  sphere3->GetProperty()->SetSpecularColor(1, 1, 1);
   sphere3->AddPosition(2.5, 0, 0);
 
   vtkSmartPointer<vtkActor> sphere4 = vtkSmartPointer<vtkActor>::New();
   sphere4->SetMapper(sphereMapper);
   sphere4->GetProperty()->SetColor(1, 0, 0);
   sphere4->GetProperty()->SetAmbient(0.3);
-  sphere4->GetProperty()->SetDiffuse(0.0);
+  sphere4->GetProperty()->SetDiffuse(0.5);
   sphere4->GetProperty()->SetSpecular(1.0);
   sphere4->GetProperty()->SetSpecularPower(40.0);
+  sphere4->GetProperty()->SetSpecularColor(1, 1, 1);
   sphere4->AddPosition(3.75, 0, 0);
 
   vtkSmartPointer<vtkActor> sphere5 = vtkSmartPointer<vtkActor>::New();
   sphere5->SetMapper(sphereMapper);
   sphere5->GetProperty()->SetColor(1, 0, 0);
   sphere5->GetProperty()->SetAmbient(0.3);
-  sphere5->GetProperty()->SetDiffuse(0.0);
+  sphere5->GetProperty()->SetDiffuse(0.5);
   sphere5->GetProperty()->SetSpecular(0.5);
   sphere5->GetProperty()->SetSpecularPower(5.0);
+  sphere5->GetProperty()->SetSpecularColor(1, 1, 1);
   sphere5->AddPosition(0.0, 1.25, 0);
 
   vtkSmartPointer<vtkActor> sphere6 = vtkSmartPointer<vtkActor>::New();
   sphere6->SetMapper(sphereMapper);
   sphere6->GetProperty()->SetColor(1, 0, 0);
   sphere6->GetProperty()->SetAmbient(0.3);
-  sphere6->GetProperty()->SetDiffuse(0.0);
+  sphere6->GetProperty()->SetDiffuse(0.5);
   sphere6->GetProperty()->SetSpecular(0.5);
   sphere6->GetProperty()->SetSpecularPower(10.0);
+  sphere6->GetProperty()->SetSpecularColor(1, 1, 1);
   sphere6->AddPosition(1.25, 1.25, 0);
 
   vtkSmartPointer<vtkActor> sphere7 = vtkSmartPointer<vtkActor>::New();
   sphere7->SetMapper(sphereMapper);
   sphere7->GetProperty()->SetColor(1, 0, 0);
   sphere7->GetProperty()->SetAmbient(0.3);
-  sphere7->GetProperty()->SetDiffuse(0.0);
+  sphere7->GetProperty()->SetDiffuse(0.5);
   sphere7->GetProperty()->SetSpecular(0.5);
   sphere7->GetProperty()->SetSpecularPower(20.0);
+  sphere7->GetProperty()->SetSpecularColor(1, 1, 1);
   sphere7->AddPosition(2.5, 1.25, 0);
 
   vtkSmartPointer<vtkActor> sphere8 = vtkSmartPointer<vtkActor>::New();
   sphere8->SetMapper(sphereMapper);
   sphere8->GetProperty()->SetColor(1, 0, 0);
   sphere8->GetProperty()->SetAmbient(0.3);
-  sphere8->GetProperty()->SetDiffuse(0.0);
+  sphere8->GetProperty()->SetDiffuse(0.5);
   sphere8->GetProperty()->SetSpecular(0.5);
   sphere8->GetProperty()->SetSpecularPower(40.0);
+  sphere8->GetProperty()->SetSpecularColor(1, 1, 1);
   sphere8->AddPosition(3.75, 1.25, 0);
 
   // Create the graphics structure. The renderer renders into the
