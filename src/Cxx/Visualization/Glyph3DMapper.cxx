9e417823c809732b66f146d23a50da694f44b103
Elvis Stansvik <elvis.stansvik@orexplore.com>
2018-04-09 21:50:57 +0200

    ENH: Show how to color/scale glyphs

--- src/Cxx/Visualization/Glyph3DMapper.cxx
+++ src/Cxx/Visualization/Glyph3DMapper.cxx
@@ -3,12 +3,17 @@
 #include <vtkCubeSource.h>
 #include <vtkPolyData.h>
 #include <vtkPoints.h>
+#include <vtkFloatArray.h>
 #include <vtkGlyph3DMapper.h>
+#include <vtkNamedColors.h>
+#include <vtkCamera.h>
 #include <vtkCellArray.h>
 #include <vtkActor.h>
+#include <vtkPointData.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkUnsignedCharArray.h>
 
 int main(int, char *[])
 {
@@ -18,9 +23,30 @@ int main(int, char *[])
   points->InsertNextPoint(1,1,1);
   points->InsertNextPoint(2,2,2);
   
+  vtkSmartPointer<vtkFloatArray> scaleFactors =
+    vtkSmartPointer<vtkFloatArray>::New();
+  scaleFactors->SetNumberOfComponents(3);
+  scaleFactors->SetName("Scale Factors");
+  scaleFactors->InsertNextTuple3(0.7, 1.0, 1.0);
+  scaleFactors->InsertNextTuple3(1.0, 0.7, 1.0);
+  scaleFactors->InsertNextTuple3(1.0, 1.0, 0.7);
+
+  vtkSmartPointer<vtkNamedColors> namedColors =
+    vtkSmartPointer<vtkNamedColors>::New();
+
+  vtkSmartPointer<vtkUnsignedCharArray> colors =
+    vtkSmartPointer<vtkUnsignedCharArray>::New();
+  colors->SetName("Colors");
+  colors->SetNumberOfComponents(3);
+  colors->InsertNextTypedTuple(namedColors->GetColor3ub("Red").GetData());
+  colors->InsertNextTypedTuple(namedColors->GetColor3ub("Green").GetData());
+  colors->InsertNextTypedTuple(namedColors->GetColor3ub("Blue").GetData());
+
   vtkSmartPointer<vtkPolyData> polydata = 
     vtkSmartPointer<vtkPolyData>::New();
   polydata->SetPoints(points);
+  polydata->GetPointData()->AddArray(colors);
+  polydata->GetPointData()->AddArray(scaleFactors);
   
   // Create anything you want here, we will use a cube for the demo.
   vtkSmartPointer<vtkCubeSource> cubeSource = 
@@ -34,6 +60,10 @@ int main(int, char *[])
 #else
   glyph3Dmapper->SetInputData(polydata);
 #endif
+  glyph3Dmapper->SetScalarModeToUsePointFieldData();
+  glyph3Dmapper->SetScaleArray("Scale Factors");
+  glyph3Dmapper->SetScaleModeToScaleByVectorComponents();
+  glyph3Dmapper->SelectColorArray("Colors");
   glyph3Dmapper->Update();
 
   vtkSmartPointer<vtkActor> actor = 
@@ -52,6 +82,10 @@ int main(int, char *[])
  
   // Add the actor to the scene
   renderer->AddActor(actor);
+
+  // Position the camera
+  renderer->GetActiveCamera()->SetPosition(-10, 5, 0);
+  renderer->GetActiveCamera()->SetFocalPoint(1, 1, 1);
   
   // Render and interact
   renderWindow->Render();
