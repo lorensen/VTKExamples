e503248ab0363462a9bb5ac7087f534ceb826bc4
chethanapavan <34049539+chethanapavan@users.noreply.github.com>
2018-12-14 14:44:16 +0900

    Updated OpenVRCone.cxx

--- src/Cxx/Visualization/OpenVRCone.cxx
+++ src/Cxx/Visualization/OpenVRCone.cxx
@@ -13,27 +13,28 @@
 int main(int, char *[])
 {
   //Create a cone
-  vtkSmartPointer<vtkConeSource> coneSource =    vtkSmartPointer<vtkConeSource>::New();
+  vtkSmartPointer<vtkConeSource> coneSource = vtkSmartPointer<vtkConeSource>::New();
   coneSource->Update();
 
   //Create a mapper and actor
-  vtkSmartPointer<vtkPolyDataMapper> mapper =    vtkSmartPointer<vtkPolyDataMapper>::New();
+  vtkSmartPointer<vtkNamedColors> colors = vtkSmartPointer<vtkNamedColors>::New();
+  vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   mapper->SetInputConnection(coneSource->GetOutputPort());
 
-  vtkSmartPointer<vtkActor> actor =    vtkSmartPointer<vtkActor>::New();
+  vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
   actor->SetMapper(mapper);
 
   //Create a renderer, render window, and interactor
-  vtkSmartPointer<vtkOpenVRRenderer> renderer =    vtkSmartPointer<vtkOpenVRRenderer>::New();
-  vtkSmartPointer<vtkOpenVRRenderWindow> renderWindow =    vtkSmartPointer<vtkOpenVRRenderWindow>::New();
+  vtkSmartPointer<vtkOpenVRRenderer> renderer = vtkSmartPointer<vtkOpenVRRenderer>::New();
+  vtkSmartPointer<vtkOpenVRRenderWindow> renderWindow = vtkSmartPointer<vtkOpenVRRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
-  vtkSmartPointer<vtkOpenVRRenderWindowInteractor> renderWindowInteractor =    vtkSmartPointer<vtkOpenVRRenderWindowInteractor>::New();
+  vtkSmartPointer<vtkOpenVRRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkOpenVRRenderWindowInteractor>::New();
   renderWindowInteractor->SetRenderWindow(renderWindow);
 
   //Add the actors to the scene
   renderer->AddActor(actor);
-  renderer->SetBackground(.3, .6, .3); // Background color green
-
+  renderer->SetBackground(colors->GetColor3d("ForestGreen").GetData());
+  
   //Render and interact
   renderWindow->Render();
   renderWindowInteractor->Start();
