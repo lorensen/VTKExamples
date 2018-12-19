db1de0d8b37e67547b4fef42a9bcf842b3862676
Bill Lorensen <bill.lorensen@gmail.com>
2018-12-18 16:08:06 -0800

    BUG: Move test images and fix formattimg

--- src/Cxx/Visualization/OpenVRCube.cxx
+++ src/Cxx/Visualization/OpenVRCube.cxx
@@ -10,38 +10,38 @@
 
 int main(int, char *[])
 {
-	// Create a cube.
-	vtkSmartPointer<vtkCubeSource> cubeSource =
-		vtkSmartPointer<vtkCubeSource>::New();
+  // Create a cube.
+  vtkSmartPointer<vtkCubeSource> cubeSource =
+    vtkSmartPointer<vtkCubeSource>::New();
 
-	// Create a mapper and actor.
-	vtkSmartPointer<vtkNamedColors> colors =
-		vtkSmartPointer<vtkNamedColors>::New();
-	vtkSmartPointer<vtkPolyDataMapper> mapper =
-		vtkSmartPointer<vtkPolyDataMapper>::New();
-	mapper->SetInputConnection(cubeSource->GetOutputPort());
+  // Create a mapper and actor.
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+  vtkSmartPointer<vtkPolyDataMapper> mapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper->SetInputConnection(cubeSource->GetOutputPort());
 
-	vtkSmartPointer<vtkActor> actor =
-		vtkSmartPointer<vtkActor>::New();
-	actor->SetMapper(mapper);
+  vtkSmartPointer<vtkActor> actor =
+    vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
 
-	// Create a renderer, render window, and interactor
-	vtkSmartPointer<vtkOpenVRRenderer> renderer =
-		vtkSmartPointer<vtkOpenVRRenderer>::New();
-	vtkSmartPointer<vtkOpenVRRenderWindow> renderWindow =
-		vtkSmartPointer<vtkOpenVRRenderWindow>::New();
-	renderWindow->AddRenderer(renderer);
-	vtkSmartPointer<vtkOpenVRRenderWindowInteractor> renderWindowInteractor =
-		vtkSmartPointer<vtkOpenVRRenderWindowInteractor>::New();
-	renderWindowInteractor->SetRenderWindow(renderWindow);
+  // Create a renderer, render window, and interactor
+  vtkSmartPointer<vtkOpenVRRenderer> renderer =
+    vtkSmartPointer<vtkOpenVRRenderer>::New();
+  vtkSmartPointer<vtkOpenVRRenderWindow> renderWindow =
+    vtkSmartPointer<vtkOpenVRRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+  vtkSmartPointer<vtkOpenVRRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkOpenVRRenderWindowInteractor>::New();
+  renderWindowInteractor->SetRenderWindow(renderWindow);
 
-	// Add the actors to the scene
-	renderer->AddActor(actor);
-	renderer->SetBackground(colors->GetColor3d("ForestGreen").GetData());
+  // Add the actors to the scene
+  renderer->AddActor(actor);
+  renderer->SetBackground(colors->GetColor3d("ForestGreen").GetData());
 
-	// Render and interact
-	renderWindow->Render();
-	renderWindowInteractor->Start();
+  // Render and interact
+  renderWindow->Render();
+  renderWindowInteractor->Start();
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
--- src/Cxx/Visualization/OpenVRCylinder.cxx
+++ src/Cxx/Visualization/OpenVRCylinder.cxx
@@ -10,41 +10,41 @@
 
 int main(int, char *argv[])
 {
-	// Create a sphere
-	vtkSmartPointer<vtkCylinderSource> cylinderSource =
-		vtkSmartPointer<vtkCylinderSource>::New();
-	cylinderSource->SetCenter(0.0, 0.0, 0.0);
-	cylinderSource->SetRadius(5.0);
-	cylinderSource->SetHeight(7.0);
-	cylinderSource->SetResolution(100);
+  // Create a sphere
+  vtkSmartPointer<vtkCylinderSource> cylinderSource =
+    vtkSmartPointer<vtkCylinderSource>::New();
+  cylinderSource->SetCenter(0.0, 0.0, 0.0);
+  cylinderSource->SetRadius(5.0);
+  cylinderSource->SetHeight(7.0);
+  cylinderSource->SetResolution(100);
 
-	// Create a mapper and actor
-	vtkSmartPointer<vtkNamedColors> colors = 
-		vtkSmartPointer<vtkNamedColors>::New();
-	vtkSmartPointer<vtkPolyDataMapper> mapper =
-		vtkSmartPointer<vtkPolyDataMapper>::New();
-	mapper->SetInputConnection(cylinderSource->GetOutputPort());
-	vtkSmartPointer<vtkActor> actor =
-		vtkSmartPointer<vtkActor>::New();
-	actor->SetMapper(mapper);
+  // Create a mapper and actor
+  vtkSmartPointer<vtkNamedColors> colors = 
+    vtkSmartPointer<vtkNamedColors>::New();
+  vtkSmartPointer<vtkPolyDataMapper> mapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper->SetInputConnection(cylinderSource->GetOutputPort());
+  vtkSmartPointer<vtkActor> actor =
+    vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
 
-	//Create a renderer, render window, and interactor
-	vtkSmartPointer<vtkOpenVRRenderer> renderer =
-		vtkSmartPointer<vtkOpenVRRenderer>::New();
-	vtkSmartPointer<vtkOpenVRRenderWindow> renderWindow =
-		vtkSmartPointer<vtkOpenVRRenderWindow>::New();
-	renderWindow->AddRenderer(renderer);
-	vtkSmartPointer<vtkOpenVRRenderWindowInteractor> renderWindowInteractor =
-		vtkSmartPointer<vtkOpenVRRenderWindowInteractor>::New();
-	renderWindowInteractor->SetRenderWindow(renderWindow);
+  //Create a renderer, render window, and interactor
+  vtkSmartPointer<vtkOpenVRRenderer> renderer =
+    vtkSmartPointer<vtkOpenVRRenderer>::New();
+  vtkSmartPointer<vtkOpenVRRenderWindow> renderWindow =
+    vtkSmartPointer<vtkOpenVRRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+  vtkSmartPointer<vtkOpenVRRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkOpenVRRenderWindowInteractor>::New();
+  renderWindowInteractor->SetRenderWindow(renderWindow);
 
-	// Add the actor to the scene
-	renderer->AddActor(actor);
-	renderer->SetBackground(colors->GetColor3d("ForestGreen").GetData());
-     // Render and interact
-	renderWindow->SetWindowName(argv[0]);
-	renderWindow->Render();
-	renderWindowInteractor->Start();
+  // Add the actor to the scene
+  renderer->AddActor(actor);
+  renderer->SetBackground(colors->GetColor3d("ForestGreen").GetData());
+  // Render and interact
+  renderWindow->SetWindowName(argv[0]);
+  renderWindow->Render();
+  renderWindowInteractor->Start();
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
