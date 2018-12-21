a842092a1a134f95b5c2fddb26f89ee278c25882
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2018-12-21 17:12:37 +0900

    Create Stripper.java

---
+++ src/Java/PolyData/Stripper.java
@@ -0,0 +1,36 @@
+import vtk.vtkSphereSource;
+import vtk.vtkStripper;
+import vtk.vtkNativeLibrary;
+
+class Stripper{  
+	
+	static {
+	    if (!vtkNativeLibrary.LoadAllNativeLibraries()) {
+	      for (vtkNativeLibrary lib : vtkNativeLibrary.values()) {
+	        if (!lib.IsLoaded()) {
+	          System.out.println(lib.GetLibraryName() + " not loaded");
+	        }
+	      }
+	    }
+	    vtkNativeLibrary.DisableOutputWindow(null);
+	  }
+	
+	
+    public static void main(String args[]){  
+    	
+    	vtkSphereSource SphereSource = new vtkSphereSource();
+		  SphereSource.Update();
+		
+		  System.out.println("Number of cells before stripping:" + SphereSource.GetOutput().GetNumberOfCells() );
+	
+			  
+	    vtkStripper Stripper = new vtkStripper();
+	    Stripper.SetInputConnection(SphereSource.GetOutputPort());
+	    Stripper.Update();
+
+			  
+		System.out.println("Number of cells after stripping:" + Stripper.GetOutput().GetNumberOfCells() );
+			  
+		  		  
+    }  
+}   
