844dfa3ab9c96dab7090cfa311b3c68a44a18d93
Andrew Maclean <andrew.amaclean@gmail.com>
2018-12-21 15:49:48 +1100

    Fixing the cylinder example.

--- src/Python/GeometricObjects/CylinderExample.py
+++ src/Python/GeometricObjects/CylinderExample.py
@@ -4,53 +4,63 @@
 # creation.
 
 import vtk
-# The colors module defines various useful colors.
-from vtk.util.colors import tomato
 
-# This creates a polygonal cylinder model with eight circumferential
-# facets.
-cylinder = vtk.vtkCylinderSource()
-cylinder.SetResolution(8)
 
-# The mapper is responsible for pushing the geometry into the graphics
-# library. It may also do color mapping, if scalars or other
-# attributes are defined.
-cylinderMapper = vtk.vtkPolyDataMapper()
-cylinderMapper.SetInputConnection(cylinder.GetOutputPort())
+def main():
+    colors = vtk.vtkNamedColors()
+    # Set the background color.
+    bkg = map(lambda x: x / 255.0, [26, 51, 102, 255])
+    colors.SetColor("BkgColor", *bkg)
 
-# The actor is a grouping mechanism: besides the geometry (mapper), it
-# also has a property, transformation matrix, and/or texture map.
-# Here we set its color and rotate it -22.5 degrees.
-cylinderActor = vtk.vtkActor()
-cylinderActor.SetMapper(cylinderMapper)
-cylinderActor.GetProperty().SetColor(tomato)
-cylinderActor.RotateX(30.0)
-cylinderActor.RotateY(-45.0)
+    # This creates a polygonal cylinder model with eight circumferential
+    # facets.
+    cylinder = vtk.vtkCylinderSource()
+    cylinder.SetResolution(8)
 
-# Create the graphics structure. The renderer renders into the render
-# window. The render window interactor captures mouse events and will
-# perform appropriate camera or actor manipulation depending on the
-# nature of the events.
-ren = vtk.vtkRenderer()
-renWin = vtk.vtkRenderWindow()
-renWin.AddRenderer(ren)
-iren = vtk.vtkRenderWindowInteractor()
-iren.SetRenderWindow(renWin)
+    # The mapper is responsible for pushing the geometry into the graphics
+    # library. It may also do color mapping, if scalars or other
+    # attributes are defined.
+    cylinderMapper = vtk.vtkPolyDataMapper()
+    cylinderMapper.SetInputConnection(cylinder.GetOutputPort())
 
-# Add the actors to the renderer, set the background and size
-ren.AddActor(cylinderActor)
-ren.SetBackground(0.1, 0.2, 0.4)
-renWin.SetSize(200, 200)
+    # The actor is a grouping mechanism: besides the geometry (mapper), it
+    # also has a property, transformation matrix, and/or texture map.
+    # Here we set its color and rotate it -22.5 degrees.
+    cylinderActor = vtk.vtkActor()
+    cylinderActor.SetMapper(cylinderMapper)
+    cylinderActor.GetProperty().SetColor(colors.GetColor3d("Tomato"))
+    cylinderActor.RotateX(30.0)
+    cylinderActor.RotateY(-45.0)
 
-# This allows the interactor to initalize itself. It has to be
-# called before an event loop.
-iren.Initialize()
+    # Create the graphics structure. The renderer renders into the render
+    # window. The render window interactor captures mouse events and will
+    # perform appropriate camera or actor manipulation depending on the
+    # nature of the events.
+    ren = vtk.vtkRenderer()
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(ren)
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(renWin)
 
-# We'll zoom in a little by accessing the camera and invoking a "Zoom"
-# method on it.
-ren.ResetCamera()
-ren.GetActiveCamera().Zoom(1.5)
-renWin.Render()
+    # Add the actors to the renderer, set the background and size
+    ren.AddActor(cylinderActor)
+    ren.SetBackground(colors.GetColor3d("BkgColor"))
+    renWin.SetSize(300, 300)
+    renWin.SetWindowName('Cylinder')
 
-# Start the event loop.
-iren.Start()
+    # This allows the interactor to initalize itself. It has to be
+    # called before an event loop.
+    iren.Initialize()
+
+    # We'll zoom in a little by accessing the camera and invoking a "Zoom"
+    # method on it.
+    ren.ResetCamera()
+    ren.GetActiveCamera().Zoom(1.5)
+    renWin.Render()
+
+    # Start the event loop.
+    iren.Start()
+
+
+if __name__ == '__main__':
+    main()
