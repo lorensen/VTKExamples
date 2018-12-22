d688786b4145cf910146eb957d8c4c065e721450
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2018-12-23 06:23:53 +0900

    Update Reflection.java

--- src/Java/PolyData/Reflection.java
+++ src/Java/PolyData/Reflection.java
@@ -9,14 +9,19 @@ import vtk.vtkDataSetMapper;
 import vtk.vtkPolyDataMapper;
 import vtk.vtkNamedColors;
 
-public class Reflection  {
+public class Reflection  
+{
 	  
 	  // -----------------------------------------------------------------
 	  // Load VTK library and print which library was not properly loaded
-	  static {
-	    if (!vtkNativeLibrary.LoadAllNativeLibraries()) {
-	      for (vtkNativeLibrary lib : vtkNativeLibrary.values()) {
-	        if (!lib.IsLoaded()) {
+	  static 
+	  {
+	    if (!vtkNativeLibrary.LoadAllNativeLibraries()) 
+	    {
+	      for (vtkNativeLibrary lib : vtkNativeLibrary.values()) 
+	      {
+	        if (!lib.IsLoaded()) 
+		{
 	          System.out.println(lib.GetLibraryName() + " not loaded");
 	        }
 	      }
@@ -25,75 +30,74 @@ public class Reflection  {
 	  }
 	  // -----------------------------------------------------------------
 			
-	  public static void main(String s[]) {
-		  
+	  public static void main(String s[]) 
+	  {
 		  vtkNamedColors Color = new vtkNamedColors(); 
 		   
-		   //For Cone Actor
-		   double ConeActorColor[] = new double[4];
+		  //For Cone Actor
+		  double ConeActorColor[] = new double[4];
 		   
-		   //For Reflection Actor
-		   double ReflectionActorColor[] = new double[4];
+		  //For Reflection Actor
+		  double ReflectionActorColor[] = new double[4];
 		   
-		   //Renderer Background Color	   
-		   double BgColor[] = new double[4];
+		  //Renderer Background Color	   
+		  double BgColor[] = new double[4];
+		   
+		  //Change Color Name to Use your own Color for Cone Actor
+		  Color.GetColor("Mint",ConeActorColor);
+		   
+		  //Change Color Name to Use your own Color for Reflection Actor
+		  Color.GetColor("Tomato",ReflectionActorColor);
 		   
 		   
-		   //Change Color Name to Use your own Color for Cone Actor
-		   Color.GetColor("Mint",ConeActorColor);
-		   
-		 //Change Color Name to Use your own Color for Reflection Actor
-		   Color.GetColor("Tomato",ReflectionActorColor);
-		   
-		   
-		   //Change Color Name to Use your own Color for Renderer Background
-		   Color.GetColor("Wheat",BgColor);
+		  //Change Color Name to Use your own Color for Renderer Background
+		  Color.GetColor("Wheat",BgColor);
 
-		   // Create a Cone
-		   vtkConeSource ConeSource = new vtkConeSource();
-		   ConeSource.Update();
+		  // Create a Cone
+		  vtkConeSource ConeSource = new vtkConeSource();
+		  ConeSource.Update();
 	
-		   // Create a mapper and actor
-		   vtkPolyDataMapper ConeMapper = new vtkPolyDataMapper();
-		   ConeMapper.SetInputConnection(ConeSource.GetOutputPort());
+		  // Create a mapper and actor
+		  vtkPolyDataMapper ConeMapper = new vtkPolyDataMapper();
+		  ConeMapper.SetInputConnection(ConeSource.GetOutputPort());
 		   
 	
-		   vtkActor ConeActor = new vtkActor();
-		   ConeActor.SetMapper(ConeMapper);
-		   ConeActor.GetProperty().SetColor(ConeActorColor);
+		  vtkActor ConeActor = new vtkActor();
+		  ConeActor.SetMapper(ConeMapper);
+		  ConeActor.GetProperty().SetColor(ConeActorColor);
 
-		   // Reflection
-		   vtkReflectionFilter ReflectionFilter = new vtkReflectionFilter();
-		   ReflectionFilter.SetInputConnection(ConeSource.GetOutputPort());
-		   ReflectionFilter.CopyInputOff();
-		   ReflectionFilter.Update();
+		  // Reflection
+		  vtkReflectionFilter ReflectionFilter = new vtkReflectionFilter();
+		  ReflectionFilter.SetInputConnection(ConeSource.GetOutputPort());
+		  ReflectionFilter.CopyInputOff();
+		  ReflectionFilter.Update();
 		   
-		   vtkDataSetMapper ReflectionMapper = new vtkDataSetMapper();
-		   ReflectionMapper.SetInputConnection(ReflectionFilter.GetOutputPort());
+		  vtkDataSetMapper ReflectionMapper = new vtkDataSetMapper();
+		  ReflectionMapper.SetInputConnection(ReflectionFilter.GetOutputPort());
 
-		   vtkActor ReflectionActor = new vtkActor();
-		   ReflectionActor.SetMapper(ReflectionMapper);
-		   ReflectionActor.GetProperty().SetColor(ReflectionActorColor);
+		  vtkActor ReflectionActor = new vtkActor();
+		  ReflectionActor.SetMapper(ReflectionMapper);
+		  ReflectionActor.GetProperty().SetColor(ReflectionActorColor);
 
 	
-		   // Create the renderer, render window and interactor.
-		   vtkRenderer ren = new vtkRenderer();
-		   vtkRenderWindow renWin = new vtkRenderWindow();
-		   renWin.AddRenderer(ren);
-		   vtkRenderWindowInteractor iren = new vtkRenderWindowInteractor();
-		   iren.SetRenderWindow(renWin);
+		  // Create the renderer, render window and interactor.
+		  vtkRenderer ren = new vtkRenderer();
+		  vtkRenderWindow renWin = new vtkRenderWindow();
+		  renWin.AddRenderer(ren);
+		  vtkRenderWindowInteractor iren = new vtkRenderWindowInteractor();
+		  iren.SetRenderWindow(renWin);
 			    
-		   // Visualise the arrow
-		   ren.AddActor(ConeActor);
-		   ren.AddActor(ReflectionActor);
-		   ren.SetBackground(BgColor);
-		   ren.ResetCamera();
+		  // Visualise the arrow
+		  ren.AddActor(ConeActor);
+		  ren.AddActor(ReflectionActor);
+		  ren.SetBackground(BgColor);
+		  ren.ResetCamera();
 		      
-		   renWin.SetSize(300,300);
-		   renWin.Render();
+		  renWin.SetSize(300,300);
+		  renWin.Render();
 
-		   iren.Initialize();
-		   iren.Start();
+		  iren.Initialize();
+		  iren.Start();
 	
 	  }
-	} 
+} 
