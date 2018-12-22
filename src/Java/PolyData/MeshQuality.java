3d8744af1f0f996a902f83f412268a767f6a4868
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2018-12-23 06:02:46 +0900

    Update MeshQuality.java

--- src/Java/PolyData/MeshQuality.java
+++ src/Java/PolyData/MeshQuality.java
@@ -33,77 +33,77 @@ public class MeshQuality
 
 	  public static void main(String s[]) 
 	  {
-		    vtkNamedColors Color = new vtkNamedColors();
+		vtkNamedColors Color = new vtkNamedColors();
 
-		    //Renderer Background Color
-		    double BgColor[] = new double[4];
+		//Renderer Background Color
+		double BgColor[] = new double[4];
 		  
-		    //Change Color Name to Use your own Color for Renderer Background
-		    Color.GetColor("NavajoWhite",BgColor);
+		//Change Color Name to Use your own Color for Renderer Background
+		Color.GetColor("NavajoWhite",BgColor);
 
-		    //Create a Sphere
-		    vtkSphereSource Sphere = new vtkSphereSource();
-		    //Sphere.SetCenter(0.0,0.0,0.0);
-		    Sphere.SetRadius(5.0);
-		    Sphere.Update();
+		//Create a Sphere
+		vtkSphereSource Sphere = new vtkSphereSource();
+		//Sphere.SetCenter(0.0,0.0,0.0);
+		phere.SetRadius(5.0);
+		Sphere.Update();
 		  
-		    //Change Phi and Theta Value for Smooth Surface
-		    Sphere.SetPhiResolution(12);
-		    Sphere.SetThetaResolution(14);
+		//Change Phi and Theta Value for Smooth Surface
+		Sphere.SetPhiResolution(12);
+		Sphere.SetThetaResolution(14);
 		    	  
-		    vtkTriangleFilter triangleFilter = new vtkTriangleFilter();
-		    triangleFilter.SetInputConnection(Sphere.GetOutputPort());
-		    triangleFilter.Update();
+		vtkTriangleFilter triangleFilter = new vtkTriangleFilter();
+		triangleFilter.SetInputConnection(Sphere.GetOutputPort());
+		triangleFilter.Update();
 		        
-		    vtkPolyData Mesh = triangleFilter.GetOutput();
-		    System.out.println("There are" + " " +  Mesh.GetNumberOfCells() +" "  + "Cells");
+		vtkPolyData Mesh = triangleFilter.GetOutput();
+		System.out.println("There are" + " " +  Mesh.GetNumberOfCells() +" "  + "Cells");
 		    
 		  
-		    vtkMeshQuality QualityFilter = new vtkMeshQuality();
-		    QualityFilter.SetInputData(Mesh);
-		    QualityFilter.SetTriangleQualityMeasureToArea();
-		    QualityFilter.Update();
+		vtkMeshQuality QualityFilter = new vtkMeshQuality();
+		QualityFilter.SetInputData(Mesh);
+		QualityFilter.SetTriangleQualityMeasureToArea();
+		QualityFilter.Update();
 		    
 	    
-		    vtkDoubleArray QualityArray = new vtkDoubleArray().FastDownCast(QualityFilter.GetOutput().GetCellData().GetArray("Quality"));
-		   System.out.println("There are" + QualityArray.GetNumberOfTuples() + "values");
+		vtkDoubleArray QualityArray = new vtkDoubleArray().FastDownCast(QualityFilter.GetOutput().GetCellData().GetArray("Quality"));
+		System.out.println("There are" + QualityArray.GetNumberOfTuples() + "values");
 		   
 		   
-		   for(int i=0;i<QualityArray.GetNumberOfTuples();i++)
-		   {
-			     double val = QualityArray.GetValue(i);
-			     System.out.println("Value" + " "+i + " " + val);
+		for(int i=0;i<QualityArray.GetNumberOfTuples();i++)
+		{
+			double val = QualityArray.GetValue(i);
+			System.out.println("Value" + " "+i + " " + val);
 			  
-		   }
+		}
 		   
-		    vtkPolyData PolyData = new vtkPolyData();
-		    PolyData.ShallowCopy(QualityFilter.GetOutput());
+		vtkPolyData PolyData = new vtkPolyData();
+		PolyData.ShallowCopy(QualityFilter.GetOutput());
 
-		    //Create a Mapper and Actor for Sphere
-		    vtkPolyDataMapper Mapper = new vtkPolyDataMapper();
-		    Mapper.SetInputData(PolyData);
+		//Create a Mapper and Actor for Sphere
+		vtkPolyDataMapper Mapper = new vtkPolyDataMapper();
+		Mapper.SetInputData(PolyData);
 		    
-		    vtkActor Actor = new vtkActor();
-		    Actor.SetMapper(Mapper);
+		vtkActor Actor = new vtkActor();
+		Actor.SetMapper(Mapper);
 		    
 		    		    
-		    // Create the renderer, render window and interactor.
-		    vtkRenderer ren = new vtkRenderer();
-		    vtkRenderWindow renWin = new vtkRenderWindow();
-		    renWin.AddRenderer(ren);
-		    vtkRenderWindowInteractor iren = new vtkRenderWindowInteractor();
-		    iren.SetRenderWindow(renWin);
+		// Create the renderer, render window and interactor.
+		vtkRenderer ren = new vtkRenderer();
+		vtkRenderWindow renWin = new vtkRenderWindow();
+		renWin.AddRenderer(ren);
+		vtkRenderWindowInteractor iren = new vtkRenderWindowInteractor();
+		iren.SetRenderWindow(renWin);
 		    
-		    // Visualise the arrow
-		    ren.AddActor(Actor);
-		    ren.SetBackground(BgColor);
-		    ren.ResetCamera();
+		 // Visualise the arrow
+		 ren.AddActor(Actor);
+		 ren.SetBackground(BgColor);
+		 ren.ResetCamera();
 	      
-		    renWin.SetSize(300,300);
-		    renWin.Render();
+		 renWin.SetSize(300,300);
+		 renWin.Render();
 
-		    iren.Initialize();
-		    iren.Start();
+		 iren.Initialize();
+		 iren.Start();
 
 	  }
 }
