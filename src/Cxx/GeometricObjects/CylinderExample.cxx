afd175a36518144ff301d26b49d27f07525edd27
Andrew Maclean <andrew.amaclean@gmail.com>
2018-12-24 13:59:03 +1100

    Adding in the renamed files.

---
+++ src/Cxx/GeometricObjects/CylinderExample.cxx
@@ -0,0 +1,74 @@
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkCylinderSource.h>
+#include <vtkNamedColors.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+
+#include <array>
+
+int main(int, char *[]) {
+  vtkSmartPointer<vtkNamedColors> colors =
+      vtkSmartPointer<vtkNamedColors>::New();
+
+  // Set the background color.
+  std::array<unsigned char, 4> bkg{{26, 51, 102, 255}};
+  colors->SetColor("BkgColor", bkg.data());
+
+  // This creates a polygonal cylinder model with eight circumferential facets
+  // (i.e, in practice an octagonal prism).
+  vtkSmartPointer<vtkCylinderSource> cylinder =
+      vtkSmartPointer<vtkCylinderSource>::New();
+  cylinder->SetResolution(8);
+
+  // The mapper is responsible for pushing the geometry into the graphics
+  // library. It may also do color mapping, if scalars or other attributes are
+  // defined.
+  vtkSmartPointer<vtkPolyDataMapper> cylinderMapper =
+      vtkSmartPointer<vtkPolyDataMapper>::New();
+  cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
+
+  // The actor is a grouping mechanism: besides the geometry (mapper), it
+  // also has a property, transformation matrix, and/or texture map.
+  // Here we set its color and rotate it around the X and Y axes.
+  vtkSmartPointer<vtkActor> cylinderActor = vtkSmartPointer<vtkActor>::New();
+  cylinderActor->SetMapper(cylinderMapper);
+  cylinderActor->GetProperty()->SetColor(
+      colors->GetColor4d("Tomato").GetData());
+  cylinderActor->RotateX(30.0);
+  cylinderActor->RotateY(-45.0);
+
+  // The renderer generates the image
+  // which is then displayed on the render window.
+  // It can be thought of as a scene to which the actor is added
+  vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+  renderer->AddActor(cylinderActor);
+  renderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
+  // Zoom in a little by accessing the camera and invoking its "Zoom" method.
+  renderer->ResetCamera();
+  renderer->GetActiveCamera()->Zoom(1.5);
+
+  // The render window is the actual GUI window
+  // that appears on the computer screen
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+      vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->SetSize(300, 300);
+  renderWindow->AddRenderer(renderer);
+  renderWindow->SetWindowName("Cylinder");
+
+  // The render window interactor captures mouse events
+  // and will perform appropriate camera or actor manipulation
+  // depending on the nature of the events.
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+      vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+
+  // This starts the event loop and as a side effect causes an initial render.
+  renderWindowInteractor->Start();
+
+  return EXIT_SUCCESS;
+}
---
+++ src/Cxx/GeometricObjects/CylinderExample.md
@@ -0,0 +1,6 @@
+### Description
+
+This example creates a minimal visualization program, demonstrating VTK's basic rendering and pipeline creation.
+
+!!! note
+    This original C++ source code for this example is [here](https://gitlab.kitware.com/vtk/vtk/blob/73465690278158b9e89661cd6aed26bead781378/Examples/Rendering/Cxx/Cylinder.cxx).
