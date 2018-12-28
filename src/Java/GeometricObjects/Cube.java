930133fc369d8a8cbca1fbb043604d6f587fb5bb
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2018-12-28 20:02:42 +0900

    Update Cube.java

--- src/Java/GeometricObjects/Cube.java
+++ src/Java/GeometricObjects/Cube.java
@@ -1,33 +1,28 @@
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-
 import vtk.vtkActor;
 import vtk.vtkNativeLibrary;
-import vtk.vtkPanel;
 import vtk.vtkDataSetMapper;
 import vtk.vtkCubeSource;
 import vtk.vtkShrinkFilter;
 import vtk.vtkProperty;
+import vtk.vtkRenderWindow;
+import vtk.vtkRenderWindowInteractor;
+import vtk.vtkRenderer;
 import vtk.vtkNamedColors;
 
 
-public class Cube extends JPanel implements ActionListener {
-  private static final long serialVersionUID = 1L;
-  private vtkPanel renWin;
-  private JButton exitButton;
+public class Cube 
+{
 
   // -----------------------------------------------------------------
   // Load VTK library and print which library was not properly loaded
-  static {
-    if (!vtkNativeLibrary.LoadAllNativeLibraries()) {
-      for (vtkNativeLibrary lib : vtkNativeLibrary.values()) {
-        if (!lib.IsLoaded()) {
+  static 
+  {
+    if (!vtkNativeLibrary.LoadAllNativeLibraries()) 
+    {
+      for (vtkNativeLibrary lib : vtkNativeLibrary.values()) 
+      {
+        if (!lib.IsLoaded()) 
+        {
           System.out.println(lib.GetLibraryName() + " not loaded");
         }
       }
@@ -35,12 +30,10 @@ public class Cube extends JPanel implements ActionListener {
     vtkNativeLibrary.DisableOutputWindow(null);
   }
   // -----------------------------------------------------------------
-	  
-	  
-  public Cube() {
-    super(new BorderLayout());
-	   
-	   
+
+  public static void main(String s[]) 
+  {	  
+
     vtkNamedColors Color = new vtkNamedColors(); 
     //For Actor Color
     double ActorColor[] = new double[4];
@@ -48,73 +41,52 @@ public class Cube extends JPanel implements ActionListener {
     double BgColor[] = new double[4];
     //BackFace color 
     double BackColor[] = new double[4];
-	   
+
     //Change Color Name to Use your own Color for Change Actor Color
     Color.GetColor("SpringGreen",ActorColor);
     //Change Color Name to Use your own Color for Renderer Background
     Color.GetColor("Red",BgColor);
     //Change Color Name to Use your own Color for BackFace Color
     Color.GetColor("Yellow",BackColor);
-	   
-	 
+
+
     // Create a cube.
     vtkCubeSource Cube = new vtkCubeSource();
-	 
+
     //shrink cells composing an arbitrary data set
     vtkShrinkFilter shrink = new vtkShrinkFilter();
     shrink.SetInputConnection(Cube.GetOutputPort());
     shrink.SetShrinkFactor(.9);
-	  
+
     //Create a Mapper and Actor
     vtkDataSetMapper Mapper = new vtkDataSetMapper();
     Mapper.SetInputConnection(shrink.GetOutputPort());
-	  
+
     vtkProperty Back = new vtkProperty();
     Back.SetColor(BackColor);
-	  
+
     vtkActor Actor = new vtkActor();
     Actor.SetMapper(Mapper);
     Actor.GetProperty().EdgeVisibilityOn();
     Actor.GetProperty().SetColor(ActorColor);
     Actor.SetBackfaceProperty(Back);
-	   
-	  
-	  
-    renWin = new vtkPanel();
-    renWin.GetRenderer().AddActor(Actor);
-    renWin.resetCamera();
-    renWin.GetRenderer().SetBackground(BgColor);
-    renWin.resetCamera();
-    renWin.resetCameraClippingRange();
-    renWin.GetRenderer().GetActiveCamera().Azimuth(30);
-    renWin.GetRenderer().GetActiveCamera().Elevation(30);
 
-    // Add Java UI components
-    exitButton = new JButton("Exit");
-    exitButton.addActionListener(this);
+    //Create the renderer, render window and interactor.
+    vtkRenderer ren = new vtkRenderer();
+    vtkRenderWindow renWin = new vtkRenderWindow();
+    renWin.AddRenderer(ren);
+    vtkRenderWindowInteractor iren = new vtkRenderWindowInteractor();
+    iren.SetRenderWindow(renWin);
 
-    add(renWin, BorderLayout.CENTER);
-    add(exitButton, BorderLayout.SOUTH);
-  }
+    // Visualise the arrow
+    ren.AddActor(Actor);
+    ren.SetBackground(BgColor);
 
-  /** An ActionListener that listens to the button. */
-  public void actionPerformed(ActionEvent e) {
-    if (e.getSource().equals(exitButton)) {
-      System.exit(0);
-    }
-  }
 
-  public static void main(String s[]) {
-    SwingUtilities.invokeLater(new Runnable() {
-        public void run() {
-          JFrame frame = new JFrame("Cube");
-          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          frame.getContentPane().setLayout(new BorderLayout());
-          frame.getContentPane().add(new Cube(), BorderLayout.CENTER);
-          frame.setSize(400, 400);
-          frame.setLocationRelativeTo(null);
-          frame.setVisible(true);
-        }
-      });
+    renWin.SetSize(300, 300);
+    renWin.Render();
+
+    iren.Initialize();
+    iren.Start();   
   }
 } 
