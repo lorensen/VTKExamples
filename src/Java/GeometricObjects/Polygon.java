c96b52f0e1fa9b0d00a3d45a2e413b47424d5731
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2018-12-30 00:57:32 +0900

    Update Polygon.java

--- src/Java/GeometricObjects/Polygon.java
+++ src/Java/GeometricObjects/Polygon.java
@@ -1,118 +1,100 @@
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-
 import vtk.vtkActor;
 import vtk.vtkCellArray;
+import vtk.vtkNamedColors;
 import vtk.vtkNativeLibrary;
-import vtk.vtkPanel;
 import vtk.vtkPoints;
 import vtk.vtkPolyData;
 import vtk.vtkPolyDataMapper;
 import vtk.vtkPolygon;
-import vtk.vtkNamedColors;
+import vtk.vtkRenderWindow;
+import vtk.vtkRenderWindowInteractor;
+import vtk.vtkRenderer;
 
-
-
-
-//Author: Bharatesh Chakravarthi
-//Affiliation: VE Lab, Chung Ang University, Seoul, South Korea. 
-
-
-public class Polygon extends JPanel implements ActionListener {
-  private static final long serialVersionUID = 1L;
-  private vtkPanel renWin;
-  private JButton exitButton;
-
-  // -----------------------------------------------------------------
-  // Load VTK library and print which library was not properly loaded
-  static {
-    if (!vtkNativeLibrary.LoadAllNativeLibraries()) {
-      for (vtkNativeLibrary lib : vtkNativeLibrary.values()) {
-        if (!lib.IsLoaded()) {
-          System.out.println(lib.GetLibraryName() + " not loaded");
-        }
-      }
-    }
-    vtkNativeLibrary.DisableOutputWindow(null);
-  }
-  // -----------------------------------------------------------------
+public class Polygon  
+{
 	  
+	// -----------------------------------------------------------------
+	// Load VTK library and print which library was not properly loaded
+	static 
+  {
+	  if (!vtkNativeLibrary.LoadAllNativeLibraries()) 
+    {
+	    for (vtkNativeLibrary lib : vtkNativeLibrary.values()) 
+      {
+	      if (!lib.IsLoaded()) 
+        {
+	        System.out.println(lib.GetLibraryName() + " not loaded");
+	      }
+	    }
+	  }
+	  vtkNativeLibrary.DisableOutputWindow(null);
+	}
+	// -----------------------------------------------------------------
 	  
-  public Polygon() {
-    super(new BorderLayout());
+
+  public static void main(String s[]) 
+  {
+
+    vtkNamedColors colors = new vtkNamedColors();
+
+    //For Actor Color
+    double actorColor[] = new double[4];
+    //Renderer Background Color
+    double Bgcolor[] = new double[4];
+
+    colors.GetColor("Red", actorColor);
+    colors.GetColor("White", Bgcolor);
 
     vtkPoints Points = new vtkPoints();
     Points.InsertNextPoint(0.0,0.0,0.0);
     Points.InsertNextPoint(1.0,0.0,0.0);
     Points.InsertNextPoint(1.0,1.0,0.0);
     Points.InsertNextPoint(0.0,1.0,0.0);
-	  
-			  
+
+          
     vtkPolygon Polygon = new vtkPolygon();
     Polygon.GetPointIds().SetNumberOfIds(4);
     Polygon.GetPointIds().SetId(0, 0);
     Polygon.GetPointIds().SetId(1, 1);
     Polygon.GetPointIds().SetId(2, 2);
     Polygon.GetPointIds().SetId(3, 3);
-	  
-		  
+
+        
     vtkCellArray Polygons = new vtkCellArray();
     Polygons.InsertNextCell(Polygon);
-	  
+
 
     vtkPolyData PolygonPolyData = new vtkPolyData();
     PolygonPolyData.SetPoints(Points);
     PolygonPolyData.SetPolys(Polygons);
-	  
-	  
+
+
     //Create a Mapper and Actor
     vtkPolyDataMapper Mapper = new vtkPolyDataMapper();
     Mapper.SetInputData(PolygonPolyData);
-	
+
 
     vtkActor Actor = new vtkActor();
     Actor.SetMapper(Mapper);
-    Actor.GetProperty().SetColor(1.0, 0.0, 0.1); 
+    Actor.GetProperty().SetColor(actorColor); 
     Actor.GetProperty().SetLineWidth(5);
-	   
-    renWin = new vtkPanel();
-	 
-    renWin.GetRenderer().AddActor(Actor);
-    renWin.resetCamera();
-    renWin.GetRenderer().SetBackground(1.0, 1.0, 1.0);
+        
+    //Create the renderer, render window and interactor.
+    vtkRenderer ren = new vtkRenderer();
+    vtkRenderWindow renWin = new vtkRenderWindow();
+    renWin.AddRenderer(ren);
+    vtkRenderWindowInteractor iren = new vtkRenderWindowInteractor();
+    iren.SetRenderWindow(renWin);
 
-    // Add Java UI components
-    exitButton = new JButton("Exit");
-    exitButton.addActionListener(this);
+    // Visualise the arrow
+    ren.AddActor(Actor);
+    ren.SetBackground(Bgcolor);
 
-    add(renWin, BorderLayout.CENTER);
-    add(exitButton, BorderLayout.SOUTH);
-  }
+    renWin.SetSize(300, 300);
+    renWin.Render();
 
-  /** An ActionListener that listens to the button. */
-  public void actionPerformed(ActionEvent e) {
-    if (e.getSource().equals(exitButton)) {
-      System.exit(0);
-    }
-  }
-
-  public static void main(String s[]) {
-    SwingUtilities.invokeLater(new Runnable() {
-        public void run() {
-          JFrame frame = new JFrame("Polygon");
-          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          frame.getContentPane().setLayout(new BorderLayout());
-          frame.getContentPane().add(new Polygon(), BorderLayout.CENTER);
-          frame.setSize(400, 400);
-          frame.setLocationRelativeTo(null);
-          frame.setVisible(true);
-        }
-      });
+    iren.Initialize();
+    iren.Start();	  
+        
   }
 } 
