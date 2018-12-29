1d40da93b83db66032b6875bffa2b3579018153c
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2018-12-30 08:48:25 +0900

    Update ExodusIIWriter.java

--- src/Java/Parallel/ExodusIIWriter.java
+++ src/Java/Parallel/ExodusIIWriter.java
@@ -4,43 +4,43 @@ import vtk.vtkTimeSourceExample;
 
 public class ExodusIIWriter 
 {
-	// -----------------------------------------------------------------
-	// Load VTK library and print which library was not properly loaded
-	static 
-	{
-		if (!vtkNativeLibrary.LoadAllNativeLibraries()) 
-		{
-			for (vtkNativeLibrary lib : vtkNativeLibrary.values()) 
-			{
-				if (!lib.IsLoaded()) 
-				{
-					System.out.println(lib.GetLibraryName() + " not loaded");
-				}
-			}
-		}
-		vtkNativeLibrary.DisableOutputWindow(null);
-	}
-	// -----------------------------------------------------------------
+// -----------------------------------------------------------------
+// Load VTK library and print which library was not properly loaded
+static 
+{
+  if (!vtkNativeLibrary.LoadAllNativeLibraries()) 
+  {
+    for (vtkNativeLibrary lib : vtkNativeLibrary.values()) 
+    {
+      if (!lib.IsLoaded()) 
+      { 
+        System.out.println(lib.GetLibraryName() + " not loaded");
+      }
+    }
+  }
+  vtkNativeLibrary.DisableOutputWindow(null);
+}
+// -----------------------------------------------------------------
 	
-	public static void main(String args[]) 
-	{					
-	  // Provide default values.
-		String fileName = "Output.exii";
-		for(int i = 0; i < args.length; ++i)
-		{
-		  switch (i) 
-		  {
-		    case 0:
-		        	fileName = args[i];
-		        	break;
+public static void main(String args[]) 
+{					
+  // Provide default values.
+  String fileName = "Output.exii";
+  for(int i = 0; i < args.length; ++i)
+  {
+    switch (i) 
+    {
+      case 0:
+      fileName = args[i];
+      break;
 
-		  }
-		}
-		vtkTimeSourceExample TimeSource = new vtkTimeSourceExample();
-		vtkExodusIIWriter ExodusWriter = new vtkExodusIIWriter();
-		ExodusWriter.SetFileName(fileName);
-		ExodusWriter.SetInputConnection(TimeSource.GetOutputPort());
-		ExodusWriter.WriteAllTimeStepsOn();
-		ExodusWriter.Write();
-	}		  
+    }
+  }
+  vtkTimeSourceExample TimeSource = new vtkTimeSourceExample();
+  vtkExodusIIWriter ExodusWriter = new vtkExodusIIWriter();
+  ExodusWriter.SetFileName(fileName);
+  ExodusWriter.SetInputConnection(TimeSource.GetOutputPort());
+  ExodusWriter.WriteAllTimeStepsOn();
+  ExodusWriter.Write();
+ }		  
 }
