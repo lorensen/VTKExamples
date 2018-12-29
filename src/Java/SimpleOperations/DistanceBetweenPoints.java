ce66178130a847b5ee8783bdbc6e816b994bc805
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2018-12-30 08:41:25 +0900

    Update DistanceBetweenPoints.java

--- src/Java/SimpleOperations/DistanceBetweenPoints.java
+++ src/Java/SimpleOperations/DistanceBetweenPoints.java
@@ -4,40 +4,40 @@ import vtk.vtkMath;
 
 public class DistanceBetweenPoints 
 {
-	// -----------------------------------------------------------------
-	// Load VTK library and print which library was not properly loaded
-	static 
-	{
-		if (!vtkNativeLibrary.LoadAllNativeLibraries()) 
-		{
-			for (vtkNativeLibrary lib : vtkNativeLibrary.values()) 
-			{
-				if (!lib.IsLoaded()) 
-				{
-					System.out.println(lib.GetLibraryName() + " not loaded");
-				}
-			}
-		}
-		vtkNativeLibrary.DisableOutputWindow(null);
+  // -----------------------------------------------------------------
+  // Load VTK library and print which library was not properly loaded
+  static 
+  {
+    if (!vtkNativeLibrary.LoadAllNativeLibraries()) 
+    {
+      for (vtkNativeLibrary lib : vtkNativeLibrary.values()) 
+      {
+        if (!lib.IsLoaded()) 
+        {
+	  System.out.println(lib.GetLibraryName() + " not loaded");
 	}
-	// -----------------------------------------------------------------
+      }
+     }
+     vtkNativeLibrary.DisableOutputWindow(null);
+   }
+   // -----------------------------------------------------------------
 	
-	public static void main(String s[]) 
-	{					  
-		// Create two points.
-		double p0[] = new double[] {0.0, 0.0, 0.0};
-		double p1[] = new double[] {1.0, 1.0, 1.0};
+public static void main(String s[]) 
+{					  
+  // Create two points.
+  double p0[] = new double[] {0.0, 0.0, 0.0};
+  double p1[] = new double[] {1.0, 1.0, 1.0};
 		
-		// Find the squared distance between the points.
-		vtkMath squaredDistance = new vtkMath();
-		double squaredDist = squaredDistance.Distance2BetweenPoints(p0, p1);
+  // Find the squared distance between the points.
+  vtkMath squaredDistance = new vtkMath();
+  double squaredDist = squaredDistance.Distance2BetweenPoints(p0, p1);
 		
-		// Take the square root to get the Euclidean distance between the points.
-		double distance = Math.sqrt(squaredDist);
+  // Take the square root to get the Euclidean distance between the points.
+  double distance = Math.sqrt(squaredDist);
 		
-		// Output the results.
-		System.out.println("Squared Distance:" + " " + squaredDist);
-		System.out.println("Distance:" + " " + distance);
+  // Output the results.
+  System.out.println("Squared Distance:" + " " + squaredDist);
+  System.out.println("Distance:" + " " + distance);
 
-	}
+ }
 }
