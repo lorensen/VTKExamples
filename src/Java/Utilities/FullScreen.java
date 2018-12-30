e1c63821005331ad75a7dc987b985bf79bd9cbfb
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2018-12-30 09:07:01 +0900

    Update FullScreen.java

--- src/Java/Utilities/FullScreen.java
+++ src/Java/Utilities/FullScreen.java
@@ -9,51 +9,51 @@ import vtk.vtkActor;
 
 public class FullScreen 
 {
-	//-----------------------------------------------------------------
-	// Load VTK library and print which library was not properly loaded
-	static 
-	{
-		if (!vtkNativeLibrary.LoadAllNativeLibraries()) 
-		{
-			for (vtkNativeLibrary lib : vtkNativeLibrary.values()) 
-			{
-				if (!lib.IsLoaded()) 
-				{
-						System.out.println(lib.GetLibraryName() + " not loaded");
-				}
-			}
-		}
-		vtkNativeLibrary.DisableOutputWindow(null);
-	}
-	//-----------------------------------------------------------------
+  //-----------------------------------------------------------------
+  // Load VTK library and print which library was not properly loaded
+  static 
+  {
+    if (!vtkNativeLibrary.LoadAllNativeLibraries()) 
+    {
+      for (vtkNativeLibrary lib : vtkNativeLibrary.values()) 
+      {
+        if (!lib.IsLoaded()) 
+        {
+          System.out.println(lib.GetLibraryName() + " not loaded");
+        }
+      }
+   }
+   vtkNativeLibrary.DisableOutputWindow(null);
+  }
+  //-----------------------------------------------------------------
 		
-	public static void main(String s[]) 
-	{
-		// Create a sphere
-	  vtkSphereSource SphereSource = new vtkSphereSource();
-	  SphereSource.Update();
+ public static void main(String s[]) 
+ {
+   // Create a sphere
+   vtkSphereSource SphereSource = new vtkSphereSource();
+   SphereSource.Update();
 	  	
-	  vtkPolyDataMapper Mapper = new vtkPolyDataMapper();
-	  Mapper.SetInputConnection(SphereSource.GetOutputPort());
+   vtkPolyDataMapper Mapper = new vtkPolyDataMapper();
+   Mapper.SetInputConnection(SphereSource.GetOutputPort());
 		
-	  vtkActor Actor = new vtkActor();
-	  Actor.SetMapper(Mapper);
+  vtkActor Actor = new vtkActor();
+  Actor.SetMapper(Mapper);
 		
-		// Create the renderer, render window and interactor.
-	  vtkRenderer ren = new vtkRenderer();
-	  vtkRenderWindow renWin = new vtkRenderWindow();
-	  renWin.AddRenderer(ren);
-	  vtkRenderWindowInteractor iren = new vtkRenderWindowInteractor();
-	  iren.SetRenderWindow(renWin);
+  // Create the renderer, render window and interactor.
+  vtkRenderer ren = new vtkRenderer();
+  vtkRenderWindow renWin = new vtkRenderWindow();
+  renWin.AddRenderer(ren);
+  vtkRenderWindowInteractor iren = new vtkRenderWindowInteractor();
+  iren.SetRenderWindow(renWin);
 	    
-	  // Visualise Sphere in full screen window
-	  ren.AddActor(Actor);
-	  renWin.Render();
-	  renWin.SetFullScreen(1);
-	  ren.ResetCamera();
-	  ren.ResetCameraClippingRange();
+  // Visualise Sphere in full screen window
+  ren.AddActor(Actor);
+  renWin.Render();
+  renWin.SetFullScreen(1);
+  ren.ResetCamera();
+  ren.ResetCameraClippingRange();
 
-	  iren.Initialize();
-	  iren.Start();
-	}
+  iren.Initialize();
+  iren.Start();
+ }
 }
