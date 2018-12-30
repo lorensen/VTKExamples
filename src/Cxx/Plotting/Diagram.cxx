3a12eea4eeb5266c1b7059fd946ba01e58104c47
Bill Lorensen <bill.lorensen@gmail.com>
2018-12-29 21:23:46 -0800

    ENH: Improved Diagram example.

--- src/Cxx/Plotting/Diagram.cxx
+++ src/Cxx/Plotting/Diagram.cxx
@@ -12,9 +12,8 @@
 #include <vtkTextProperty.h>
 #include <vtkOpenGLContextDevice2D.h>
 #include <vtkStdString.h>
-
-#define VTK_CREATE(type, name) \
-  vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
+#include <vtkColorSeries.h>
+#include <vtkNamedColors.h>
 
 //----------------------------------------------------------------------------
 class APIDiagram : public vtkContextItem
@@ -22,32 +21,70 @@ class APIDiagram : public vtkContextItem
 public:
   static APIDiagram *New();
   vtkTypeMacro(APIDiagram, vtkContextItem);
+  APIDiagram()
+  {
+    this->ColorSeries = vtkSmartPointer<vtkColorSeries>::New();
+    this->ColorSchemeName = "Brewer Diverging Spectral (7)";
+    this->ColorSeries->SetColorSchemeByName(this->ColorSchemeName);
+  }
   // Paint event for the chart, called whenever the chart needs to be drawn
   virtual bool Paint(vtkContext2D *painter);
+  void SetColorSchemeName (std::string seriesName)
+  {
+    this->ColorSeries->SetColorSchemeByName(seriesName);
+    if (this->ColorSeries->GetColorScheme() > vtkColorSeries::BREWER_QUALITATIVE_SET3)
+    {
+      this->PrintColorSchemes();
+    }
+  }
+  void PrintColorSchemes(ostream& os = std::cout)
+  {
+    int saveId = this->ColorSeries->GetColorScheme();
+    for (unsigned int i = 0; i < vtkColorSeries::BREWER_QUALITATIVE_SET3; ++i)
+    {
+      this->ColorSeries->SetColorScheme(i);
+      os << this->ColorSeries->GetColorSchemeName() << std::endl;
+    }
+    this->ColorSeries->SetColorScheme(saveId);
+  }
+protected:
+  vtkSmartPointer<vtkColorSeries> ColorSeries;
+  std::string ColorSchemeName;
 };
 
 //----------------------------------------------------------------------------
-int main( int, char *[] )
+int main( int argc, char *argv[] )
 {
+  std::string colorSchemeName = "Brewer Diverging Brown-Blue-Green (7)";
+  if (argc > 1)
+  {
+    colorSchemeName = std::string(argv[1]);
+  }
+
   // Set up a 2D chart actor, APIDiagram object andn add them to the renderer
-  VTK_CREATE(vtkContextActor, actor);
-  VTK_CREATE(APIDiagram, diagram);
+  vtkNew<APIDiagram>  diagram;
+  diagram->SetColorSchemeName(colorSchemeName);
+
+  vtkNew<vtkContextActor>  actor;
   actor->GetScene()->AddItem(diagram);
-  VTK_CREATE(vtkRenderer, renderer);
-  renderer->SetBackground(.2, .3, .4);
-  VTK_CREATE(vtkRenderWindow, renderWindow);
+
+  vtkNew<vtkNamedColors> colors;
+  vtkNew<vtkRenderer>  renderer;
+  renderer->SetBackground(colors->GetColor3d("Tan").GetData());
+
+  vtkNew<vtkRenderWindow>  renderWindow;
   renderWindow->SetSize(800, 600);
   renderWindow->AddRenderer(renderer);
   renderer->AddActor(actor);
 
-  VTK_CREATE(vtkRenderWindowInteractor, interactor);
+  vtkNew<vtkRenderWindowInteractor>  interactor;
   interactor->SetRenderWindow(renderWindow);
   renderWindow->SetMultiSamples(0);
   renderWindow->Render();
 
   interactor->Start();
 
-  return true;
+  return EXIT_SUCCESS;
 }
 
 // Make our new derived class to draw a diagram
@@ -62,33 +99,41 @@ bool APIDiagram::Paint(vtkContext2D *painter)
   painter->GetTextProp()->SetFontSize(24);
   painter->GetPen()->SetColor(0, 0, 0);
 
-  painter->GetBrush()->SetColor(100, 255, 100);
+  unsigned int c = 0;
+  painter->GetBrush()->SetColor((this->ColorSeries->GetColorRepeating(c)).GetData());
+  c++;
+
   painter->DrawRect(100, 50, 200, 100);
   painter->DrawString(200, 100, "OpenGL");
 
-  painter->GetBrush()->SetColor(255, 100, 0);
+  painter->GetBrush()->SetColor((this->ColorSeries->GetColorRepeating(c)).GetData());
+  c++;
   painter->DrawRect(300, 50, 200, 100);
   painter->DrawString(400, 100, "Others?");
 
-  painter->GetBrush()->SetColor(100, 0, 255);
+  painter->GetBrush()->SetColor((this->ColorSeries->GetColorRepeating(c)).GetData());
+  c++;
   painter->DrawRect(500, 50, 200, 100);
   painter->DrawString(600, 100, "Others?");
 
-  painter->GetBrush()->SetColor(180, 180, 255);
+  painter->GetBrush()->SetColor((this->ColorSeries->GetColorRepeating(c)).GetData());
+  c++;
   painter->DrawRect(100, 150, 600, 100);
   painter->DrawString(400, 200, "2D API");
 
-  painter->GetBrush()->SetColor(255, 255, 180);
+  painter->GetBrush()->SetColor((this->ColorSeries->GetColorRepeating(c)).GetData());
+  c++;
   painter->DrawRect(100, 250, 600, 200);
   painter->DrawString(400, 400, "Canvas API");
 
-  painter->GetBrush()->SetColor(180, 255, 180);
+  painter->GetBrush()->SetColor((this->ColorSeries->GetColorRepeating(c)).GetData());
+  c++;
   painter->DrawRect(100, 250, 300, 100);
   painter->DrawString(250, 300, "Point Mark");
 
-  painter->GetBrush()->SetColor(255, 255, 255);
+  painter->GetBrush()->SetColor((this->ColorSeries->GetColorRepeating(c)).GetData());
   painter->DrawRect(100, 450, 600, 100);
   painter->DrawString(400, 500, "Canvas View");
 
-  return true;
+return true;
 }
