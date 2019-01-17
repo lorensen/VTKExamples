4f240a0ad2e1e3e39a1b9575fb9fe6b1990529c0
Bill Lorensen <bill.lorensen@gmail.com>
2019-01-16 21:57:06 -0800

    ENH: Add include files to C++ files that include other C++ files
    WhatModule needs to find include vtk files. Source files like
    RotationsA.cxx include Rotations.cxx and do not have any include vtk
    files. This MR adds the proper includes to those files.
    
    	modified:   src/Cxx/Visualization/FlyingFrogSkinAndTissueB.cxx

--- src/Cxx/Rendering/WalkCowB.cxx
+++ src/Cxx/Rendering/WalkCowB.cxx
@@ -1,1 +1,19 @@
+// These includes are necessary for WhatModulesVTK.py
+#include <vtkActor.h>
+#include <vtkAxes.h>
+#include <vtkBYUReader.h>
+#include <vtkCallbackCommand.h>
+#include <vtkCamera.h>
+#include <vtkNamedColors.h>
+#include <vtkPNGWriter.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+#include <vtkTransform.h>
+#include <vtkVersionMacros.h>
+#include <vtkWindowToImageFilter.h>
+
 #include "WalkCow.cxx"
--- src/Cxx/VisualizationAlgorithms/MarchingCasesC.cxx
+++ src/Cxx/VisualizationAlgorithms/MarchingCasesC.cxx
@@ -1,1 +1,26 @@
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkContourFilter.h>
+#include <vtkCubeSource.h>
+#include <vtkExtractEdges.h>
+#include <vtkFloatArray.h>
+#include <vtkGlyph3D.h>
+#include <vtkIdList.h>
+#include <vtkNamedColors.h>
+#include <vtkPointData.h>
+#include <vtkPoints.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkShrinkPolyData.h>
+#include <vtkSphereSource.h>
+#include <vtkThresholdPoints.h>
+#include <vtkTransform.h>
+#include <vtkTransformPolyDataFilter.h>
+#include <vtkTubeFilter.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkVectorText.h>
+
 #include "MarchingCases.cxx"
