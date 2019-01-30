dd4fe17d93e03efff816a4ff3f6fefa1dfcb1d9f
Andrew J. P. Maclean <andrew.amaclean@gmail.com>
2019-01-30 14:48:40 +1100

    Completing transition to QVTKOpenGLNativeWidget

--- src/Cxx/Qt/EventQtSlotConnectDriver.cxx
+++ src/Cxx/Qt/EventQtSlotConnectDriver.cxx
@@ -5,7 +5,7 @@
 int main(int argc, char** argv)
 {
   // needed to ensure appropriate OpenGL context is created for VTK rendering.
-  QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
+  QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
 
   QApplication app(argc, argv);
 
