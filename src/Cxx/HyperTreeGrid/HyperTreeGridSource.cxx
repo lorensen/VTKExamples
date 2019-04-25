c88470fd4b98d694526a7929937a448101e942a3
Andrew J. P. Maclean <andrew.amaclean@gmail.com>
2019-04-26 09:45:48 +1000

    Fixes for HyperTreeGridSource

--- src/Cxx/HyperTreeGrid/HyperTreeGridSource.cxx
+++ src/Cxx/HyperTreeGrid/HyperTreeGridSource.cxx
@@ -1,56 +1,77 @@
 #include <vtkHyperTreeGridSource.h>
-
-#include <vtkShrinkFilter.h>
+#include <vtkHyperTreeGridToUnstructuredGrid.h>
 
 #include <vtkActor.h>
 #include <vtkCamera.h>
-#include <vtkProperty.h>
 #include <vtkDataSetMapper.h>
+#include <vtkNamedColors.h>
+#include <vtkPolyData.h>
+#include <vtkProperty.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
-#include <vtkNamedColors.h>
+#include <vtkShrinkFilter.h>
+#include <vtkSmartPointer.h>
 
-int main( int, char*[] )
+#include <cstdlib>
+
+int main(int, char*[])
 {
   // Create hyper tree grid source
-  vtkSmartPointer<vtkHyperTreeGridSource> source =
-    vtkSmartPointer<vtkHyperTreeGridSource>::New();
-  source->SetMaximumLevel( 6 );
-  source->SetGridSize( 3, 3, 2 );
-  source->SetGridScale( 1.5, 1., .7 );
-  source->SetDimension( 3 );
-  source->SetBranchFactor( 4 );
-  source->SetDescriptor( "RRR .R. .RR ..R ..R .R.|R.......................... ........................... ........................... .............R............. ....RR.RR........R......... .....RRRR.....R.RR......... ........................... ........................... ...........................|........................... ........................... ........................... ...RR.RR.......RR.......... ........................... RR......................... ........................... ........................... ........................... ........................... ........................... ........................... ........................... ............RRR............|........................... ........................... .......RR.................. ........................... ........................... ........................... ........................... ........................... ........................... ........................... ...........................|........................... ..........................." );
+  auto source = vtkSmartPointer<vtkHyperTreeGridSource>::New();
+  source->SetMaximumLevel(6);
+  source->SetDimensions(4, 4, 3); // GridCell 3, 3, 2
+  source->SetGridScale(1.5, 1.0, 0.7);
+  source->SetBranchFactor(4);
+  source->SetDescriptor(
+      "RRR .R. .RR ..R ..R .R.|R.......................... "
+      "........................... ........................... "
+      ".............R............. ....RR.RR........R......... "
+      ".....RRRR.....R.RR......... ........................... "
+      "........................... "
+      "...........................|........................... "
+      "........................... ........................... "
+      "...RR.RR.......RR.......... ........................... "
+      "RR......................... ........................... "
+      "........................... ........................... "
+      "........................... ........................... "
+      "........................... ........................... "
+      "............RRR............|........................... "
+      "........................... .......RR.................. "
+      "........................... ........................... "
+      "........................... ........................... "
+      "........................... ........................... "
+      "........................... "
+      "...........................|........................... "
+      "...........................");
   source->Update();
 
-  vtkSmartPointer<vtkShrinkFilter> shrink =
-    vtkSmartPointer<vtkShrinkFilter>::New();
-  shrink->SetInputConnection(source->GetOutputPort());
-  shrink->SetShrinkFactor( .8 );
+  // Hyper tree grid to unstructured grid filter
+  auto htg2ug = vtkSmartPointer<vtkHyperTreeGridToUnstructuredGrid>::New();
+  htg2ug->SetInputConnection(source->GetOutputPort());
+  htg2ug->Update();
 
-  vtkSmartPointer<vtkDataSetMapper> mapper =
-    vtkSmartPointer<vtkDataSetMapper>::New();
+  auto shrink = vtkSmartPointer<vtkShrinkFilter>::New();
+  shrink->SetInputConnection(htg2ug->GetOutputPort());
+  shrink->SetShrinkFactor(0.8);
+
+  auto mapper = vtkSmartPointer<vtkDataSetMapper>::New();
   mapper->SetInputConnection(shrink->GetOutputPort());
   mapper->ScalarVisibilityOff();
 
-  vtkSmartPointer<vtkNamedColors> colors =
-    vtkSmartPointer<vtkNamedColors>::New();
+  auto colors = vtkSmartPointer<vtkNamedColors>::New();
 
-  vtkSmartPointer<vtkActor> actor =
-    vtkSmartPointer<vtkActor>::New();
+  auto actor = vtkSmartPointer<vtkActor>::New();
   actor->SetMapper(mapper);
-  actor->GetProperty()->SetDiffuseColor(colors->GetColor3d("Burlywood").GetData());
+  actor->GetProperty()->SetDiffuseColor(
+      colors->GetColor3d("Burlywood").GetData());
 
-// Create the RenderWindow, Renderer and both Actors
-//
-  vtkSmartPointer<vtkRenderer> renderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
+  // Create the RenderWindow, Renderer and both Actors
+  //
+  auto renderer = vtkSmartPointer<vtkRenderer>::New();
+  auto renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
-  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  auto interactor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
   interactor->SetRenderWindow(renderWindow);
 
   renderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
@@ -61,6 +82,8 @@ int main( int, char*[] )
   renderer->ResetCameraClippingRange();
 
   renderWindow->SetSize(640, 480);
+  renderWindow->Render();
+  renderWindow->SetWindowName("HyperTreeGridSource");
   interactor->Start();
   return EXIT_SUCCESS;
-  }
+}
