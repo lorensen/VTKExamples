edbf8eaaa59fc8572823312bf5e790367b1a978e
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2019-06-14 16:33:05 +0900

    Update TexturedSphere.cxx

--- src/Cxx/Texture/TexturedSphere.cxx
+++ src/Cxx/Texture/TexturedSphere.cxx
@@ -17,7 +17,7 @@ int main (int argc, char *argv[])
   if (argc < 2)
   {
       std::cout << "Usage: " << argv[0]
-                << " texture(.png)"
+                << " texture(.png/.ppm) e.g. earth.ppm"
                 << " [translate]" <<std::endl;
       return EXIT_FAILURE;
   }
