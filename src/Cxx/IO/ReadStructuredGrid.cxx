50b71e82ab9f8391c696d2959acc11cd243413f1
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2019-06-17 14:32:51 +0900

    Update ReadStructuredGrid.cxx

--- src/Cxx/IO/ReadStructuredGrid.cxx
+++ src/Cxx/IO/ReadStructuredGrid.cxx
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
   if ( argc != 2 )
   {
     std::cout << "Usage: " << argv[0]
-              << " Filename.vts" << std::endl;
+              << " Filename.vts e.g. StructuredGrid.vts" << std::endl;
     return EXIT_FAILURE;
   }
 
