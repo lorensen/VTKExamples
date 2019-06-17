3eca012948ba51e833fd8dc0498620f8a982e7ee
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2019-06-17 14:28:30 +0900

    Update ReadPNM.cxx

--- src/Cxx/IO/ReadPNM.cxx
+++ src/Cxx/IO/ReadPNM.cxx
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
   if ( argc != 2 )
   {
     std::cout << "Usage: " << argv[0]
-              << " Filename(.pnm)" << std::endl;
+              << " Filename(.pnm) e.g Gourds.pnm" << std::endl;
     return EXIT_FAILURE;
   }
 
