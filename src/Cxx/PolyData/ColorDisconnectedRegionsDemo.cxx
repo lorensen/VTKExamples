56f76b374ffba5608088d9b27b2beed260de0350
Bill Lorensen <bill.lorensen@gmail.com>
2019-07-22 14:51:31 -0700

    ENH: Added more colors and modernized.

--- src/Cxx/PolyData/ColorDisconnectedRegionsDemo.cxx
+++ src/Cxx/PolyData/ColorDisconnectedRegionsDemo.cxx
@@ -9,6 +9,7 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleTrackballCamera.h>
 #include <vtkLookupTable.h>
 #include <vtkNamedColors.h>
 
@@ -20,20 +21,23 @@
 #include <vtkXMLPolyDataReader.h>
 
 #include <vtksys/SystemTools.hxx>
+#include <random>
 
 namespace
 {
 vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName);
+void RandomColors(vtkSmartPointer<vtkLookupTable> &lut, int numberOfColors);
 }
 
 int main(int argc, char*argv[])
 {
-  vtkSmartPointer<vtkPolyData> polyData = ReadPolyData(argc > 1 ? argv[1] : "");;
+  vtkSmartPointer<vtkPolyData> polyData =
+    ReadPolyData(argc > 1 ? argv[1] : "");
 
-  vtkSmartPointer<vtkNamedColors> colors =
+  auto colors =
     vtkSmartPointer<vtkNamedColors>::New();
   
-  vtkSmartPointer<vtkPolyDataConnectivityFilter> connectivityFilter =
+  auto connectivityFilter =
     vtkSmartPointer<vtkPolyDataConnectivityFilter>::New();
   connectivityFilter->SetInputData(polyData);
   connectivityFilter->SetExtractionModeToAllRegions();
@@ -41,18 +45,26 @@ int main(int argc, char*argv[])
   connectivityFilter->Update();
 
   // Visualize
-  vtkSmartPointer<vtkPolyDataMapper> mapper =
+  auto numberOfRegions = connectivityFilter->GetNumberOfExtractedRegions();
+  auto lut =
+    vtkSmartPointer<vtkLookupTable>::New();
+  lut->SetNumberOfTableValues(std::max(numberOfRegions, 10));
+  lut->Build();
+  RandomColors(lut, numberOfRegions);
+  auto mapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
   mapper->SetInputConnection(connectivityFilter->GetOutputPort());
   mapper->SetScalarRange(connectivityFilter->GetOutput()->GetPointData()->GetArray("RegionId")->GetRange());
+  mapper->SetLookupTable(lut);
   mapper->Update();
 
-  vtkSmartPointer<vtkActor> actor =
+  auto actor =
     vtkSmartPointer<vtkActor>::New();
   actor->SetMapper(mapper);
 
-  vtkSmartPointer<vtkRenderer> renderer =
+  auto renderer =
     vtkSmartPointer<vtkRenderer>::New();
+  renderer->UseHiddenLineRemovalOn();
   renderer->AddActor(actor);
   renderer->SetBackground(colors->GetColor3d("Silver").GetData());
 
@@ -63,13 +75,16 @@ int main(int argc, char*argv[])
   renderer->GetActiveCamera()->Dolly(1.2);
   renderer->ResetCameraClippingRange();
 
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
+  auto renderWindow =
     vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
   renderWindow->SetSize(640, 480);
 
-  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+  auto style =
+    vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
+  auto interactor =
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  interactor->SetInteractorStyle(style);
   interactor->SetRenderWindow(renderWindow);
   renderWindow->Render();
   interactor->Initialize();
@@ -86,7 +101,7 @@ vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName)
   std::string extension = vtksys::SystemTools::GetFilenameLastExtension(std::string(fileName));
   if (extension == ".ply")
   {
-    vtkSmartPointer<vtkPLYReader> reader =
+    auto reader =
       vtkSmartPointer<vtkPLYReader>::New();
     reader->SetFileName (fileName);
     reader->Update();
@@ -94,7 +109,7 @@ vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName)
   }
   else if (extension == ".vtp")
   {
-    vtkSmartPointer<vtkXMLPolyDataReader> reader =
+    auto reader =
       vtkSmartPointer<vtkXMLPolyDataReader>::New();
     reader->SetFileName (fileName);
     reader->Update();
@@ -102,7 +117,7 @@ vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName)
   }
   else if (extension == ".obj")
   {
-    vtkSmartPointer<vtkOBJReader> reader =
+    auto reader =
       vtkSmartPointer<vtkOBJReader>::New();
     reader->SetFileName (fileName);
     reader->Update();
@@ -110,7 +125,7 @@ vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName)
   }
   else if (extension == ".stl")
   {
-    vtkSmartPointer<vtkSTLReader> reader =
+    auto reader =
       vtkSmartPointer<vtkSTLReader>::New();
     reader->SetFileName (fileName);
     reader->Update();
@@ -118,7 +133,7 @@ vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName)
   }
   else if (extension == ".vtk")
   {
-    vtkSmartPointer<vtkPolyDataReader> reader =
+    auto reader =
       vtkSmartPointer<vtkPolyDataReader>::New();
     reader->SetFileName (fileName);
     reader->Update();
@@ -126,7 +141,7 @@ vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName)
   }
   else if (extension == ".g")
   {
-    vtkSmartPointer<vtkBYUReader> reader =
+    auto reader =
       vtkSmartPointer<vtkBYUReader>::New();
     reader->SetGeometryFileName (fileName);
     reader->Update();
@@ -134,11 +149,40 @@ vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName)
   }
   else
   {
-    vtkSmartPointer<vtkSphereSource> source =
+    auto source =
       vtkSmartPointer<vtkSphereSource>::New();
     source->Update();
     polyData = source->GetOutput();
   }
   return polyData;
+}
+
+void RandomColors(vtkSmartPointer<vtkLookupTable> &lut, int numberOfColors)
+{
+  // Fill in a few known colors, the rest will be generated if needed
+  auto colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+  lut->SetTableValue(0, colors->GetColor4d("Gold").GetData());
+  lut->SetTableValue(1, colors->GetColor4d("Banana").GetData());
+  lut->SetTableValue(2, colors->GetColor4d("Tomato").GetData());
+  lut->SetTableValue(3, colors->GetColor4d("Wheat").GetData());
+  lut->SetTableValue(4, colors->GetColor4d("Lavender").GetData());
+  lut->SetTableValue(5, colors->GetColor4d("Flesh").GetData());
+  lut->SetTableValue(6, colors->GetColor4d("Raspberry").GetData());
+  lut->SetTableValue(7, colors->GetColor4d("Salmon").GetData());
+  lut->SetTableValue(8, colors->GetColor4d("Mint").GetData());
+  lut->SetTableValue(9, colors->GetColor4d("Peacock").GetData());
+
+  // If the number of color is larger than the number of specified colors,
+  // generate some random colors.
+  if (numberOfColors > 9)
+  {
+    std::mt19937 mt(4355412); //Standard mersenne_twister_engine
+    std::uniform_real_distribution<double> distribution(.6, 1.0);
+    for (auto i = 10; i < numberOfColors; ++i)
+    {
+      lut->SetTableValue(i, distribution(mt), distribution(mt), distribution(mt), 1.0);
+    }
+  }
 }
 }
