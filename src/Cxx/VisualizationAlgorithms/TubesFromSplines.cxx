3b46873ef5f6ffe0c3772d9d8e27d26c632d70e9
Bill Lorensen <bill.lorensen@gmail.com>
2019-07-22 21:35:55 -0700

    ENH: Modernize

--- src/Cxx/VisualizationAlgorithms/TubesFromSplines.cxx
+++ src/Cxx/VisualizationAlgorithms/TubesFromSplines.cxx
@@ -11,14 +11,16 @@
 #include <vtkPointData.h>
 
 #include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
 #include <vtkActor.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkNamedColors.h>
 
 int main(int, char *[])
 {
-  vtkSmartPointer<vtkPoints> points =
+  auto points =
     vtkSmartPointer<vtkPoints>::New();
   points->InsertPoint(0,1,0,0);
   points->InsertPoint(1,2,0,0);
@@ -28,10 +30,10 @@ int main(int, char *[])
   points->InsertPoint(5,6,0,0);
 
   // Fit a spline to the points
-  vtkSmartPointer<vtkParametricSpline> spline =
+  auto spline =
     vtkSmartPointer<vtkParametricSpline>::New();
   spline->SetPoints(points);
-  vtkSmartPointer<vtkParametricFunctionSource> functionSource =
+  auto functionSource =
     vtkSmartPointer<vtkParametricFunctionSource>::New();
   functionSource->SetParametricFunction(spline);
   functionSource->SetUResolution(10 * points->GetNumberOfPoints());
@@ -39,7 +41,7 @@ int main(int, char *[])
 
   // Interpolate the scalars
   double rad;
-  vtkSmartPointer<vtkTupleInterpolator> interpolatedRadius =
+  auto interpolatedRadius =
     vtkSmartPointer<vtkTupleInterpolator> ::New();
   interpolatedRadius->SetInterpolationTypeToLinear();
   interpolatedRadius->SetNumberOfComponents(1);
@@ -51,30 +53,30 @@ int main(int, char *[])
   rad = .1; interpolatedRadius->AddTuple(5,&rad);
 
   // Generate the radius scalars
-  vtkSmartPointer<vtkDoubleArray> tubeRadius =
+  auto tubeRadius =
     vtkSmartPointer<vtkDoubleArray>::New();
   unsigned int n = functionSource->GetOutput()->GetNumberOfPoints();
   tubeRadius->SetNumberOfTuples(n);
   tubeRadius->SetName("TubeRadius");
-  double tMin = interpolatedRadius->GetMinimumT();
-  double tMax = interpolatedRadius->GetMaximumT();
+  auto tMin = interpolatedRadius->GetMinimumT();
+  auto tMax = interpolatedRadius->GetMaximumT();
   double r;
   for (unsigned int i = 0; i < n; ++i)
   {
-    double t = (tMax - tMin) / (n - 1) * i + tMin;
+    auto t = (tMax - tMin) / (n - 1) * i + tMin;
     interpolatedRadius->InterpolateTuple(t, &r);
     tubeRadius->SetTuple1(i, r);
   }
 
   // Add the scalars to the polydata
-  vtkSmartPointer<vtkPolyData> tubePolyData =
+  auto tubePolyData =
     vtkSmartPointer<vtkPolyData>::New();
   tubePolyData = functionSource->GetOutput();
   tubePolyData->GetPointData()->AddArray(tubeRadius);
   tubePolyData->GetPointData()->SetActiveScalars("TubeRadius");
 
   // Create the tubes
-  vtkSmartPointer<vtkTubeFilter> tuber =
+  auto tuber =
     vtkSmartPointer<vtkTubeFilter>::New();
   tuber->SetInputData(tubePolyData);
   tuber->SetNumberOfSides(20);
@@ -82,33 +84,40 @@ int main(int, char *[])
 
   //--------------
   // Setup actors and mappers
-  vtkSmartPointer<vtkPolyDataMapper> lineMapper =
+  auto lineMapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
   lineMapper->SetInputData(tubePolyData);
   lineMapper->SetScalarRange(tubePolyData->GetScalarRange());
 
-  vtkSmartPointer<vtkPolyDataMapper> tubeMapper =
+  auto tubeMapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
   tubeMapper->SetInputConnection(tuber->GetOutputPort());
   tubeMapper->SetScalarRange(tubePolyData->GetScalarRange());
 
-  vtkSmartPointer<vtkActor> lineActor = vtkSmartPointer<vtkActor>::New();
+  auto lineActor = vtkSmartPointer<vtkActor>::New();
   lineActor->SetMapper(lineMapper);
-  vtkSmartPointer<vtkActor> tubeActor = vtkSmartPointer<vtkActor>::New();
+  lineActor->GetProperty()->SetLineWidth(3);
+  auto tubeActor = vtkSmartPointer<vtkActor>::New();
   tubeActor->SetMapper(tubeMapper);
+  tubeActor->GetProperty()->SetOpacity(.6);
 
   // Setup render window, renderer, and interactor
-  vtkSmartPointer<vtkRenderer> renderer =
+  auto colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+  auto renderer =
     vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
+  renderer->UseHiddenLineRemovalOn();
+  auto renderWindow =
     vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+  renderWindow->SetSize(640, 480);
+
+  auto renderWindowInteractor =
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
   renderWindowInteractor->SetRenderWindow(renderWindow);
   renderer->AddActor(lineActor);
   renderer->AddActor(tubeActor);
-  renderer->SetBackground(.4, .5, .6);
+  renderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
 
   renderWindow->Render();
   renderWindowInteractor->Start();
