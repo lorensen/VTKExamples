a6510114d8a8d6bde8d7676ed9825969079a7ae0
Andrew Maclean <andrew.amaclean@gmail.com>
2019-07-25 17:09:47 +1000

    Running CppCheck and modernising any of the changed ImageData files

--- src/Cxx/ImageData/ClipVolume.cxx
+++ src/Cxx/ImageData/ClipVolume.cxx
@@ -1,12 +1,10 @@
 // This program draws a checkerboard and clips it with two planes.
 
-#include <vtkSmartPointer.h>
-
-#include <vtkClipVolume.h>
-
 #include <vtkActor.h>
 #include <vtkCamera.h>
 #include <vtkCellData.h>
+#include <vtkClipVolume.h>
+#include <vtkColor.h>
 #include <vtkDataSetMapper.h>
 #include <vtkDoubleArray.h>
 #include <vtkExecutive.h>
@@ -15,28 +13,26 @@
 #include <vtkInteractorStyleSwitch.h>
 #include <vtkLookupTable.h>
 #include <vtkMapper.h>
+#include <vtkNamedColors.h>
 #include <vtkPlanes.h>
 #include <vtkPointData.h>
 #include <vtkPoints.h>
 #include <vtkProperty.h>
-#include <vtkImageData.h>
+#include <vtkRenderStepsPass.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
-#include <vtkRenderStepsPass.h>
+#include <vtkSmartPointer.h>
 
-#include <vtkNamedColors.h>
-#include <vtkColor.h>
-
-#define IMAGESIZE 64		// number of checkerboard squares on a side
-#define CUBESIZE 20.0		// physical linear dimension of entire system
+#define IMAGESIZE 64  // number of checkerboard squares on a side
+#define CUBESIZE 20.0 // physical linear dimension of entire system
 
 // Color for the checkerboard image
 #define IMAGE_R 0.7
 #define IMAGE_G 0.7
 #define IMAGE_B 0.7
 #define IMAGE_A 1.0
-#define DIM 0.5			// amount to dim the dark squares by
+#define DIM 0.5 // amount to dim the dark squares by
 
 // Offsets for clipping planes with normals in the X and Y directions
 #define XOFFSET 8
@@ -44,77 +40,42 @@
 
 ///////////////////
 
-// Make a image data.A checkerboard pattern is used for
+namespace {
+
+// Make the image data. A checkerboard pattern is used for
 // simplicity.
+vtkSmartPointer<vtkImageData> makeImage(int n, vtkColor3d fillColor,
+                                        vtkColor3d checkerColor);
+} // namespace
 
-vtkSmartPointer<vtkImageData> makeImage(int n, vtkColor3d fillColor, vtkColor3d checkerColor) {
-  vtkSmartPointer<vtkImageData> image0 = vtkSmartPointer<vtkImageData>::New();
-  image0->SetDimensions(n, n, n);
-  image0->AllocateScalars(VTK_UNSIGNED_CHAR, 1);
-  image0->SetSpacing(CUBESIZE/n, CUBESIZE/n, CUBESIZE/n);
-  int checkerSize = n / 8;
-  for(int z=0; z<n; z++) {
-    for(int y=0; y<n; y++) {
-      for(int x=0; x<n; x++) {
-	unsigned char *ptr = (unsigned char*) image0->GetScalarPointer(x, y, z);
-	*ptr = (x/checkerSize+y/checkerSize+z/checkerSize)%2; // checkerboard
-      }
-    }
-  }
-  
-  vtkSmartPointer<vtkLookupTable> lut = vtkSmartPointer<vtkLookupTable>::New();
-  lut->SetNumberOfTableValues(2);
-  lut->SetTableRange(0, 1);
-  lut->SetTableValue(0, fillColor.GetRed(), fillColor.GetGreen(), fillColor.GetBlue(), 1.0);
-  lut->SetTableValue(1, checkerColor.GetRed(), checkerColor.GetGreen(), checkerColor.GetBlue(), 1.0);
-
-  vtkSmartPointer<vtkImageMapToColors> map =
-    vtkSmartPointer<vtkImageMapToColors>::New();
-  map->SetLookupTable(lut);
-  map->SetOutputFormatToRGBA();
-  map->SetInputData(image0);
-  map->GetExecutive()->Update();
-  vtkImageData *image = map->GetOutput();
-
-  vtkPointData *pointData = image->GetPointData();
-  return image;
-}
-
-/////////////////////
-////////////////////
-
-int main(int, char *[]) {
+int main(int, char*[])
+{
 
   vtkMapper::SetResolveCoincidentTopologyToPolygonOffset();
-  
+
   // Define colors
-  vtkSmartPointer<vtkNamedColors> colors =
-    vtkSmartPointer<vtkNamedColors>::New();
+  auto colors = vtkSmartPointer<vtkNamedColors>::New();
   vtkColor3d backgroundColor = colors->GetColor3d("Wheat");
   vtkColor3d checkerColor = colors->GetColor3d("Tomato");
   vtkColor3d fillColor = colors->GetColor3d("Banana");
 
-  vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+  auto renderer = vtkSmartPointer<vtkRenderer>::New();
   renderer->SetBackground(backgroundColor.GetData());
 
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
+  auto renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
   renderWindow->SetSize(640, 480);
 
-  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  vtkSmartPointer<vtkInteractorStyleSwitch> style =
-    vtkSmartPointer<vtkInteractorStyleSwitch>::New();
+  auto interactor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  auto style = vtkSmartPointer<vtkInteractorStyleSwitch>::New();
   interactor->SetInteractorStyle(style);
   interactor->SetRenderWindow(renderWindow);
 
-  vtkSmartPointer<vtkImageData> image = makeImage(IMAGESIZE, fillColor, checkerColor);
+  auto image = makeImage(IMAGESIZE, fillColor, checkerColor);
 
   // Clipping planes in the X and Y direction.
-  vtkSmartPointer<vtkDoubleArray> normals
-    = vtkSmartPointer<vtkDoubleArray>::New();
-  vtkSmartPointer<vtkPoints> clipPts = vtkSmartPointer<vtkPoints>::New();
+  auto normals = vtkSmartPointer<vtkDoubleArray>::New();
+  auto clipPts = vtkSmartPointer<vtkPoints>::New();
   normals->SetNumberOfComponents(3);
   double xnorm[3] = {-1., 0., 0.};
   double ynorm[3] = {0., -1., 0.};
@@ -124,18 +85,16 @@ int main(int, char *[]) {
   normals->InsertNextTuple(ynorm);
   clipPts->InsertNextPoint(xpt);
   clipPts->InsertNextPoint(ypt);
-  vtkSmartPointer<vtkPlanes> clipPlanes = vtkSmartPointer<vtkPlanes>::New();
+  auto clipPlanes = vtkSmartPointer<vtkPlanes>::New();
   clipPlanes->SetNormals(normals);
   clipPlanes->SetPoints(clipPts);
 
-  vtkSmartPointer<vtkClipVolume> clipper =
-    vtkSmartPointer<vtkClipVolume>::New();
+  auto clipper = vtkSmartPointer<vtkClipVolume>::New();
   clipper->SetClipFunction(clipPlanes);
   clipper->SetInputData(image);
 
-  vtkSmartPointer<vtkDataSetMapper> imageMapper =
-    vtkSmartPointer<vtkDataSetMapper>::New();
-  vtkSmartPointer<vtkActor> imageActor = vtkSmartPointer<vtkActor>::New();
+  auto imageMapper = vtkSmartPointer<vtkDataSetMapper>::New();
+  auto imageActor = vtkSmartPointer<vtkActor>::New();
   imageActor->SetMapper(imageMapper);
   renderer->AddViewProp(imageActor);
   imageMapper->SetInputConnection(clipper->GetOutputPort());
@@ -145,7 +104,49 @@ int main(int, char *[]) {
   renderer->GetActiveCamera()->Elevation(30);
   renderer->ResetCameraClippingRange();
   renderWindow->Render();
-  
+
   interactor->Start();
   return 0;
 }
+
+namespace {
+// Make the image data. A checkerboard pattern is used for
+// simplicity.
+vtkSmartPointer<vtkImageData> makeImage(int n, vtkColor3d fillColor,
+                                        vtkColor3d checkerColor)
+{
+  auto image0 = vtkSmartPointer<vtkImageData>::New();
+  image0->SetDimensions(n, n, n);
+  image0->AllocateScalars(VTK_UNSIGNED_CHAR, 1);
+  image0->SetSpacing(CUBESIZE / n, CUBESIZE / n, CUBESIZE / n);
+  int checkerSize = n / 8;
+  for (int z = 0; z < n; z++)
+  {
+    for (int y = 0; y < n; y++)
+    {
+      for (int x = 0; x < n; x++)
+      {
+        unsigned char* ptr = (unsigned char*)image0->GetScalarPointer(x, y, z);
+        *ptr = (x / checkerSize + y / checkerSize + z / checkerSize) %
+            2; // checkerboard
+      }
+    }
+  }
+
+  auto lut = vtkSmartPointer<vtkLookupTable>::New();
+  lut->SetNumberOfTableValues(2);
+  lut->SetTableRange(0, 1);
+  lut->SetTableValue(0, fillColor.GetRed(), fillColor.GetGreen(),
+                     fillColor.GetBlue(), 1.0);
+  lut->SetTableValue(1, checkerColor.GetRed(), checkerColor.GetGreen(),
+                     checkerColor.GetBlue(), 1.0);
+
+  auto map = vtkSmartPointer<vtkImageMapToColors>::New();
+  map->SetLookupTable(lut);
+  map->SetOutputFormatToRGBA();
+  map->SetInputData(image0);
+  map->GetExecutive()->Update();
+
+  return  map->GetOutput();
+}
+} // namespace
--- src/Cxx/ImageProcessing/EnhanceEdges.cxx
+++ src/Cxx/ImageProcessing/EnhanceEdges.cxx
@@ -1,11 +1,12 @@
+#include <vector>
 #include <vtkCamera.h>
 #include <vtkDataArray.h>
 #include <vtkImageActor.h>
 #include <vtkImageCast.h>
 #include <vtkImageData.h>
 #include <vtkImageLaplacian.h>
-#include <vtkImageMapper3D.h>
 #include <vtkImageMapToWindowLevelColors.h>
+#include <vtkImageMapper3D.h>
 #include <vtkImageMathematics.h>
 #include <vtkImageProperty.h>
 #include <vtkImageReader2.h>
@@ -13,58 +14,52 @@
 #include <vtkImageThreshold.h>
 #include <vtkInteractorStyleImage.h>
 #include <vtkPointData.h>
-#include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
 #include <vtkSmartPointer.h>
-#include <vector>
 
-int main (int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   // Verify input arguments
-  if ( argc != 2 )
+  if (argc != 2)
   {
-    std::cout << "Usage: " << argv[0]
-              << " Filename" << std::endl;
+    std::cout << "Usage: " << argv[0] << " Filename" << std::endl;
     return EXIT_FAILURE;
   }
 
   // Read the image
-  vtkSmartPointer<vtkImageReader2Factory> readerFactory =
-    vtkSmartPointer<vtkImageReader2Factory>::New();
+  auto readerFactory = vtkSmartPointer<vtkImageReader2Factory>::New();
   vtkSmartPointer<vtkImageReader2> reader;
-  reader.TakeReference(
-    readerFactory->CreateImageReader2(argv[1]));
+  reader.TakeReference(readerFactory->CreateImageReader2(argv[1]));
   reader->SetFileName(argv[1]);
   reader->Update();
 
   int scalarRange[2];
   scalarRange[0] =
-    reader->GetOutput()->GetPointData()->GetScalars()->GetRange()[0];
+      reader->GetOutput()->GetPointData()->GetScalars()->GetRange()[0];
   scalarRange[1] =
-    reader->GetOutput()->GetPointData()->GetScalars()->GetRange()[1];
-  std::cout << "Range: "
-            << scalarRange[0] << ", " << scalarRange[1]
+      reader->GetOutput()->GetPointData()->GetScalars()->GetRange()[1];
+  std::cout << "Range: " << scalarRange[0] << ", " << scalarRange[1]
             << std::endl;
-  int middleSlice = (reader->GetOutput()->GetExtent()[5] -
-                     reader->GetOutput()->GetExtent()[4]) / 2;
+  // int middleSlice = (reader->GetOutput()->GetExtent()[5] -
+  //                   reader->GetOutput()->GetExtent()[4]) /
+  //    2;
 
-  middleSlice = 22;
+  // Better to use this value.
+  auto middleSlice = 22;
 
   // Work with triple images
-  vtkSmartPointer<vtkImageCast> cast =
-    vtkSmartPointer<vtkImageCast>::New();
+  auto cast = vtkSmartPointer<vtkImageCast>::New();
   cast->SetInputConnection(reader->GetOutputPort());
   cast->SetOutputScalarTypeToDouble();
   cast->Update();
 
-  vtkSmartPointer<vtkImageLaplacian> laplacian =
-    vtkSmartPointer<vtkImageLaplacian>::New();
+  auto laplacian = vtkSmartPointer<vtkImageLaplacian>::New();
   laplacian->SetInputConnection(cast->GetOutputPort());
   laplacian->SetDimensionality(3);
 
-  vtkSmartPointer<vtkImageMathematics> enhance =
-    vtkSmartPointer<vtkImageMathematics>::New();
+  auto enhance = vtkSmartPointer<vtkImageMathematics>::New();
   enhance->SetInputConnection(0, cast->GetOutputPort());
   enhance->SetInputConnection(1, laplacian->GetOutputPort());
   enhance->SetOperationToSubtract();
@@ -73,57 +68,51 @@ int main (int argc, char *argv[])
   int colorLevel = colorWindow / 2;
 
   // Map the image through the lookup table
-  vtkSmartPointer<vtkImageMapToWindowLevelColors> originalColor =
-    vtkSmartPointer<vtkImageMapToWindowLevelColors>::New();
+  auto originalColor = vtkSmartPointer<vtkImageMapToWindowLevelColors>::New();
   originalColor->SetWindow(colorWindow);
   originalColor->SetLevel(colorLevel);
   originalColor->SetInputConnection(reader->GetOutputPort());
 
-  vtkSmartPointer<vtkImageActor> originalActor =
-    vtkSmartPointer<vtkImageActor>::New();
-  originalActor->GetMapper()->SetInputConnection(originalColor->GetOutputPort());
+  auto originalActor = vtkSmartPointer<vtkImageActor>::New();
+  originalActor->GetMapper()->SetInputConnection(
+      originalColor->GetOutputPort());
   originalActor->GetProperty()->SetInterpolationTypeToNearest();
   originalActor->SetDisplayExtent(
-    reader->GetDataExtent()[0], reader->GetDataExtent()[1],
-    reader->GetDataExtent()[2], reader->GetDataExtent()[3],
-    middleSlice, middleSlice);
+      reader->GetDataExtent()[0], reader->GetDataExtent()[1],
+      reader->GetDataExtent()[2], reader->GetDataExtent()[3], middleSlice,
+      middleSlice);
 
-  vtkSmartPointer<vtkImageMapToWindowLevelColors> laplacianColor =
-    vtkSmartPointer<vtkImageMapToWindowLevelColors>::New();
+  auto laplacianColor = vtkSmartPointer<vtkImageMapToWindowLevelColors>::New();
   laplacianColor->SetWindow(1000);
   laplacianColor->SetLevel(0);
   laplacianColor->SetInputConnection(laplacian->GetOutputPort());
 
-  vtkSmartPointer<vtkImageActor> laplacianActor =
-    vtkSmartPointer<vtkImageActor>::New();
-  laplacianActor->GetMapper()->SetInputConnection(laplacianColor->GetOutputPort());
+  auto laplacianActor = vtkSmartPointer<vtkImageActor>::New();
+  laplacianActor->GetMapper()->SetInputConnection(
+      laplacianColor->GetOutputPort());
   laplacianActor->GetProperty()->SetInterpolationTypeToNearest();
   laplacianActor->SetDisplayExtent(originalActor->GetDisplayExtent());
 
-  vtkSmartPointer<vtkImageMapToWindowLevelColors> enhancedColor =
-    vtkSmartPointer<vtkImageMapToWindowLevelColors>::New();
+  auto enhancedColor = vtkSmartPointer<vtkImageMapToWindowLevelColors>::New();
   enhancedColor->SetWindow(colorWindow);
   enhancedColor->SetLevel(colorLevel);
   enhancedColor->SetInputConnection(enhance->GetOutputPort());
 
-  vtkSmartPointer<vtkImageActor> enhancedActor =
-    vtkSmartPointer<vtkImageActor>::New();
-  enhancedActor->GetMapper()->SetInputConnection(enhancedColor->GetOutputPort());
+  auto enhancedActor = vtkSmartPointer<vtkImageActor>::New();
+  enhancedActor->GetMapper()->SetInputConnection(
+      enhancedColor->GetOutputPort());
   enhancedActor->GetProperty()->SetInterpolationTypeToNearest();
   enhancedActor->SetDisplayExtent(originalActor->GetDisplayExtent());
 
   // Setup renderers
-  vtkSmartPointer<vtkRenderer> originalRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
+  auto originalRenderer = vtkSmartPointer<vtkRenderer>::New();
   originalRenderer->AddActor(originalActor);
-  vtkSmartPointer<vtkRenderer> laplacianRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
+  auto laplacianRenderer = vtkSmartPointer<vtkRenderer>::New();
   laplacianRenderer->AddActor(laplacianActor);
-  vtkSmartPointer<vtkRenderer> enhancedRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
+  auto enhancedRenderer = vtkSmartPointer<vtkRenderer>::New();
   enhancedRenderer->AddActor(enhancedActor);
 
-  std::vector<vtkSmartPointer<vtkRenderer> > renderers;
+  std::vector<vtkSmartPointer<vtkRenderer>> renderers;
   renderers.push_back(originalRenderer);
   renderers.push_back(laplacianRenderer);
   renderers.push_back(enhancedRenderer);
@@ -133,30 +122,28 @@ int main (int argc, char *argv[])
   unsigned int xGridDimensions = 3;
   unsigned int yGridDimensions = 1;
 
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
-  renderWindow->SetSize(
-    rendererSize * xGridDimensions, rendererSize * yGridDimensions);
+  auto renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->SetSize(rendererSize * xGridDimensions,
+                        rendererSize * yGridDimensions);
   for (int row = 0; row < static_cast<int>(yGridDimensions); row++)
   {
     for (int col = 0; col < static_cast<int>(xGridDimensions); col++)
     {
       int index = row * xGridDimensions + col;
       // (xmin, ymin, xmax, ymax)
-       double viewport[4] = {
-         static_cast<double>(col) / xGridDimensions,
-         static_cast<double>(yGridDimensions - (row + 1)) / yGridDimensions,
-         static_cast<double>(col + 1) / xGridDimensions,
-         static_cast<double>(yGridDimensions - row) / yGridDimensions};
+      double viewport[4] = {
+          static_cast<double>(col) / xGridDimensions,
+          static_cast<double>(yGridDimensions - (row + 1)) / yGridDimensions,
+          static_cast<double>(col + 1) / xGridDimensions,
+          static_cast<double>(yGridDimensions - row) / yGridDimensions};
       renderers[index]->SetViewport(viewport);
       renderWindow->AddRenderer(renderers[index]);
     }
   }
 
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  vtkSmartPointer<vtkInteractorStyleImage> style =
-    vtkSmartPointer<vtkInteractorStyleImage>::New();
+  auto renderWindowInteractor =
+      vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  auto style = vtkSmartPointer<vtkInteractorStyleImage>::New();
 
   renderWindowInteractor->SetInteractorStyle(style);
   renderWindowInteractor->SetRenderWindow(renderWindow);
