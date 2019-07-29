8dba1db16e3b799c8249325367a1093ca0b1e1e7
Andrew Maclean <andrew.amaclean@gmail.com>
2019-07-29 15:33:19 +1000

    Running CppCheck on the Rendering files

--- src/Cxx/Rendering/Rainbow.cxx
+++ src/Cxx/Rendering/Rainbow.cxx
@@ -22,77 +22,79 @@
 // Note: The Update method is manually invoked because it causes the
 // reader to read; later on we use the output of the reader to set
 // a range for the scalar values.
-int main (int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   if (argc < 3)
   {
-    std::cout << "Usage: " << argv[0] << " file.xyz file.bin" << std::endl;
+    std::cout << "Usage: " << argv[0]
+              << " filexyz.bin fileq.bin e.g.: combxyz.bin combq.bin"
+              << std::endl;
     return EXIT_FAILURE;
   }
   vtkSmartPointer<vtkNamedColors> colors =
-    vtkSmartPointer<vtkNamedColors>::New();
+      vtkSmartPointer<vtkNamedColors>::New();
 
   vtkSmartPointer<vtkMultiBlockPLOT3DReader> pl3d =
-    vtkSmartPointer<vtkMultiBlockPLOT3DReader>::New();
+      vtkSmartPointer<vtkMultiBlockPLOT3DReader>::New();
   pl3d->SetXYZFileName(argv[1]);
   pl3d->SetQFileName(argv[2]);
   pl3d->SetScalarFunctionNumber(100);
   pl3d->SetVectorFunctionNumber(202);
   pl3d->Update();
 
-  vtkStructuredGrid *pl3dOutput =
-    dynamic_cast<vtkStructuredGrid*>(pl3d->GetOutput()->GetBlock(0));
+  vtkStructuredGrid* pl3dOutput =
+      dynamic_cast<vtkStructuredGrid*>(pl3d->GetOutput()->GetBlock(0));
 
   std::vector<vtkSmartPointer<vtkRenderer>> renderers;
 
   // Create the 4 renderers
   vtkSmartPointer<vtkRenderer> grayScaleRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
+      vtkSmartPointer<vtkRenderer>::New();
   renderers.push_back(grayScaleRenderer);
   vtkSmartPointer<vtkRenderer> rainbowBlueRedRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
+      vtkSmartPointer<vtkRenderer>::New();
   renderers.push_back(rainbowBlueRedRenderer);
   vtkSmartPointer<vtkRenderer> rainbowRedBlueRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
+      vtkSmartPointer<vtkRenderer>::New();
   renderers.push_back(rainbowRedBlueRenderer);
   vtkSmartPointer<vtkRenderer> largeContrastRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
+      vtkSmartPointer<vtkRenderer>::New();
   renderers.push_back(largeContrastRenderer);
 
-  std::vector<vtkSmartPointer<vtkLookupTable> > luts;
+  std::vector<vtkSmartPointer<vtkLookupTable>> luts;
 
   vtkSmartPointer<vtkLookupTable> grayScaleLut =
-    vtkSmartPointer<vtkLookupTable>::New();
+      vtkSmartPointer<vtkLookupTable>::New();
   grayScaleLut->SetHueRange(0, 0);
   grayScaleLut->SetSaturationRange(0, 0);
   grayScaleLut->SetValueRange(0.2, 1.0);
   grayScaleLut->SetNumberOfColors(256);
-  grayScaleLut->SetHueRange(0.0, 0.667 );
+  grayScaleLut->SetHueRange(0.0, 0.667);
   grayScaleLut->Build();
   luts.push_back(grayScaleLut);
 
   vtkSmartPointer<vtkLookupTable> rainbowBlueRedLut =
-    vtkSmartPointer<vtkLookupTable>::New();
+      vtkSmartPointer<vtkLookupTable>::New();
   rainbowBlueRedLut->SetNumberOfColors(256);
   rainbowBlueRedLut->SetHueRange(0.667, 0.0);
   rainbowBlueRedLut->Build();
   luts.push_back(rainbowBlueRedLut);
 
   vtkSmartPointer<vtkLookupTable> rainbowRedBlueLut =
-    vtkSmartPointer<vtkLookupTable>::New();
+      vtkSmartPointer<vtkLookupTable>::New();
   rainbowRedBlueLut->SetNumberOfColors(256);
   rainbowRedBlueLut->SetHueRange(0.0, 0.667);
   rainbowRedBlueLut->Build();
   luts.push_back(rainbowRedBlueLut);
 
   vtkSmartPointer<vtkLookupTable> highContrastLut =
-    vtkSmartPointer<vtkLookupTable>::New();
+      vtkSmartPointer<vtkLookupTable>::New();
   highContrastLut->SetNumberOfColors(256);
   highContrastLut->Build();
 
   for (int l = 0; l < 16; ++l)
   {
-    highContrastLut->SetTableValue(l * 16,     1, 0, 0, 1);
+    highContrastLut->SetTableValue(l * 16, 1, 0, 0, 1);
     highContrastLut->SetTableValue(l * 16 + 1, 0, 1, 0, 1);
     highContrastLut->SetTableValue(l * 16 + 2, 0, 0, 1, 1);
     highContrastLut->SetTableValue(l * 16 + 3, 0, 0, 0, 1);
@@ -100,9 +102,9 @@ int main (int argc, char *argv[])
   luts.push_back(highContrastLut);
 
   vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
+      vtkSmartPointer<vtkRenderWindow>::New();
   vtkSmartPointer<vtkRenderWindowInteractor> interactor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+      vtkSmartPointer<vtkRenderWindowInteractor>::New();
   interactor->SetRenderWindow(renderWindow);
 
   int rendererSize = 256;
@@ -117,43 +119,41 @@ int main (int argc, char *argv[])
       int index = row * xGridDimensions + col;
       // (xmin, ymin, xmax, ymax)
       double viewport[4] = {
-        static_cast<double>(col) / xGridDimensions,
-        static_cast<double>(yGridDimensions - (row + 1)) / yGridDimensions,
-        static_cast<double>(col + 1) / xGridDimensions,
-        static_cast<double>(yGridDimensions - row) / yGridDimensions};
+          static_cast<double>(col) / xGridDimensions,
+          static_cast<double>(yGridDimensions - (row + 1)) / yGridDimensions,
+          static_cast<double>(col + 1) / xGridDimensions,
+          static_cast<double>(yGridDimensions - row) / yGridDimensions};
       renderers[index]->SetViewport(viewport);
     }
   }
 
   vtkSmartPointer<vtkStructuredGridGeometryFilter> plane =
-    vtkSmartPointer<vtkStructuredGridGeometryFilter>::New();
+      vtkSmartPointer<vtkStructuredGridGeometryFilter>::New();
   plane->SetInputData(pl3dOutput);
-  plane->SetExtent( 1, 100, 1, 100, 7, 7);
+  plane->SetExtent(1, 100, 1, 100, 7, 7);
 
   for (size_t r = 0; r < renderers.size(); ++r)
   {
     vtkSmartPointer<vtkLookupTable> lut = luts[r];
     vtkSmartPointer<vtkPolyDataMapper> planeMapper =
-      vtkSmartPointer<vtkPolyDataMapper>::New();
+        vtkSmartPointer<vtkPolyDataMapper>::New();
     planeMapper->SetLookupTable(lut);
     planeMapper->SetInputConnection(plane->GetOutputPort());
     planeMapper->SetScalarRange(pl3dOutput->GetScalarRange());
 
-    vtkSmartPointer<vtkActor> planeActor =
-      vtkSmartPointer<vtkActor>::New();
+    vtkSmartPointer<vtkActor> planeActor = vtkSmartPointer<vtkActor>::New();
     planeActor->SetMapper(planeMapper);
 
     // This creates an outline around the data.
     vtkSmartPointer<vtkStructuredGridOutlineFilter> outline =
-      vtkSmartPointer<vtkStructuredGridOutlineFilter>::New();
+        vtkSmartPointer<vtkStructuredGridOutlineFilter>::New();
     outline->SetInputData(pl3dOutput);
 
     vtkSmartPointer<vtkPolyDataMapper> outlineMapper =
-      vtkSmartPointer<vtkPolyDataMapper>::New();
+        vtkSmartPointer<vtkPolyDataMapper>::New();
     outlineMapper->SetInputConnection(outline->GetOutputPort());
 
-    vtkSmartPointer<vtkActor> outlineActor =
-      vtkSmartPointer<vtkActor>::New();
+    vtkSmartPointer<vtkActor> outlineActor = vtkSmartPointer<vtkActor>::New();
     outlineActor->SetMapper(outlineMapper);
 
     // Create the RenderWindow, Renderer and both Actors
@@ -173,8 +173,8 @@ int main (int argc, char *argv[])
     renderWindow->AddRenderer(renderers[r]);
   }
 
-  vtkSmartPointer<vtkLookupTable> lut =
-    vtkSmartPointer<vtkLookupTable>::New();
+  // vtkSmartPointer<vtkLookupTable> lut =
+  //  vtkSmartPointer<vtkLookupTable>::New();
 
   // Add the actors to the renderer, set the background and size
   //
@@ -184,10 +184,10 @@ int main (int argc, char *argv[])
 
   interactor->Initialize();
 
-  vtkCamera *cam1 = renderers[0]->GetActiveCamera();
-  cam1->SetClippingRange( 3.95297, 50);
-  cam1->SetFocalPoint( 8.88908, 0.595038, 29.3342);
-  cam1->SetPosition( -12.3332, 31.7479, 41.2387);
+  vtkCamera* cam1 = renderers[0]->GetActiveCamera();
+  cam1->SetClippingRange(3.95297, 50);
+  cam1->SetFocalPoint(8.88908, 0.595038, 29.3342);
+  cam1->SetPosition(-12.3332, 31.7479, 41.2387);
   cam1->SetViewUp(0.060772, -0.319905, 0.945498);
 
   renderWindow->Render();
