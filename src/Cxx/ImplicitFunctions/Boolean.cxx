52404ea1b9aa447a048475384bc0c3b2de6c7f21
chethana <34049539+chethanapavan@users.noreply.github.com>
2019-08-07 16:14:41 +0900

    Update Boolean.cxx

--- src/Cxx/ImplicitFunctions/Boolean.cxx
+++ src/Cxx/ImplicitFunctions/Boolean.cxx
@@ -14,80 +14,78 @@
 #include <vtkColor.h>
 
 int main(int argc, char *argv[])
-
 {
 	// Define colors
-	vtkSmartPointer<vtkNamedColors> colors =
-		vtkSmartPointer<vtkNamedColors>::New();
-	vtkColor3d actorColor = colors->GetColor3d("AliceBlue");
-	vtkColor3d  EdgeColour = colors->GetColor3d("SteelBlue");
-	vtkColor3d BackgroundColour = colors->GetColor3d("Silver");
-
-	// create a sphere
-	vtkSmartPointer<vtkSphere> sphere =
-		vtkSmartPointer<vtkSphere>::New();
-    sphere->SetCenter(1.0, 0.0, 0.0);
-	sphere->SetRadius(1);
-
-	//create a box
-	vtkSmartPointer<vtkBox> box =
-		vtkSmartPointer<vtkBox>::New();
-	box->SetBounds(-1, 1, -1, 1, -1, 1);
-
-	// combine the two implicit functions
-	vtkSmartPointer<vtkImplicitBoolean> boolean =
-		vtkSmartPointer<vtkImplicitBoolean>::New();
-	boolean->SetOperationTypeToDifference();
-	// boolean->SetOperationTypeToUnion()
-	// boolean->SetOperationTypeToIntersection()
-	boolean->AddFunction(box);
-	boolean->AddFunction(sphere);
-	//The sample function generates a distance function from the implicit
-	//function.This is then contoured to get a polygonal surface.
-	vtkSmartPointer<vtkSampleFunction> sample =
-		vtkSmartPointer<vtkSampleFunction>::New();
-	sample->SetImplicitFunction(boolean);
-	sample->SetModelBounds(-1, 2, -1, 1, -1, 1);
-	sample->SetSampleDimensions(40, 40, 40);
-	sample->ComputeNormalsOff();
-
-	// contour
-	vtkSmartPointer<vtkContourFilter> surface =
-		vtkSmartPointer<vtkContourFilter>::New();
-	surface->SetInputConnection(sample->GetOutputPort());
-	surface->SetValue(0, 0.0);
-
-	//Create a mapper and an actor
-	vtkSmartPointer<vtkPolyDataMapper> mapper =
-		vtkSmartPointer<vtkPolyDataMapper>::New();
-	mapper->SetInputConnection(surface->GetOutputPort());
-	mapper->ScalarVisibilityOff();
-  vtkSmartPointer<vtkActor> actor =
-		vtkSmartPointer<vtkActor>::New();
-	actor->SetMapper(mapper);
-	actor->GetProperty()->EdgeVisibilityOn();
-	actor->GetProperty()->SetColor(actorColor.GetData());
-	actor->GetProperty()->SetEdgeColor(EdgeColour.GetData());
-
-	
-	// A renderer and render window
-	vtkSmartPointer<vtkRenderer> renderer =
-		vtkSmartPointer<vtkRenderer>::New();
-	renderer->SetBackground(BackgroundColour.GetData());
-	vtkSmartPointer<vtkRenderWindow> renderWindow =
-		vtkSmartPointer<vtkRenderWindow>::New();
-	renderWindow->AddRenderer(renderer);
-	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
-		vtkSmartPointer<vtkRenderWindowInteractor>::New();
-	renderWindowInteractor->SetRenderWindow(renderWindow);
-	//add the actor
-	renderer->AddActor(actor);
-	// Start
-	renderer->GetActiveCamera()->SetPosition(5.0, -4.0, 1.6);
-	renderer->GetActiveCamera()->SetViewUp(0.1, 0.5, 0.9);
-	renderer->GetActiveCamera()->SetDistance(6.7);
-
-	renderWindow->Render();
-	renderWindowInteractor->Start();
-
+  auto colors =
+  	vtkSmartPointer<vtkNamedColors>::New();
+  vtkColor3d actorColor = colors->GetColor3d("AliceBlue");
+  vtkColor3d  EdgeColour = colors->GetColor3d("SteelBlue");
+  vtkColor3d BackgroundColour = colors->GetColor3d("Silver");
+  
+  // create a sphere
+  auto sphere =
+  	vtkSmartPointer<vtkSphere>::New();
+  sphere->SetCenter(1.0, 0.0, 0.0);
+  sphere->SetRadius(1);
+  
+  //create a box
+  auto box =
+  	vtkSmartPointer<vtkBox>::New();
+  box->SetBounds(-1, 1, -1, 1, -1, 1);
+  
+  // combine the two implicit functions
+  auto boolean =
+  	vtkSmartPointer<vtkImplicitBoolean>::New();
+  boolean->SetOperationTypeToDifference();
+  // boolean->SetOperationTypeToUnion()
+  // boolean->SetOperationTypeToIntersection()
+  boolean->AddFunction(box);
+  boolean->AddFunction(sphere);
+  //The sample function generates a distance function from the implicit
+  //function.This is then contoured to get a polygonal surface.
+  auto sample =
+  	vtkSmartPointer<vtkSampleFunction>::New();
+  sample->SetImplicitFunction(boolean);
+  sample->SetModelBounds(-1, 2, -1, 1, -1, 1);
+  sample->SetSampleDimensions(40, 40, 40);
+  sample->ComputeNormalsOff();
+  
+  // contour
+  auto surface =
+  	vtkSmartPointer<vtkContourFilter>::New();
+  surface->SetInputConnection(sample->GetOutputPort());
+  surface->SetValue(0, 0.0);
+  
+  //Create a mapper and an actor
+  auto mapper =
+  	vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper->SetInputConnection(surface->GetOutputPort());
+  mapper->ScalarVisibilityOff();
+  auto actor =
+  	vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
+  actor->GetProperty()->EdgeVisibilityOn();
+  actor->GetProperty()->SetColor(actorColor.GetData());
+  actor->GetProperty()->SetEdgeColor(EdgeColour.GetData());
+  
+  
+  // A renderer and render window
+  auto renderer =
+  	vtkSmartPointer<vtkRenderer>::New();
+  renderer->SetBackground(BackgroundColour.GetData());
+  auto renderWindow =
+  	vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+  auto renderWindowInteractor =
+  	vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+  //add the actor
+  renderer->AddActor(actor);
+  // Start
+  renderer->GetActiveCamera()->SetPosition(5.0, -4.0, 1.6);
+  renderer->GetActiveCamera()->SetViewUp(0.1, 0.5, 0.9);
+  renderer->GetActiveCamera()->SetDistance(6.7);
+  
+  renderWindow->Render();
+  renderWindowInteractor->Start();
 }
